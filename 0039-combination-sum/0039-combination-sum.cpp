a39b3b5d6d39e87c5f09778160fbc397d7981241
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2024-08-11 12:45:33 +0530

    Time: 0 ms (100%), Space: 12.6 MB (64.98%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,26 @@
 class Solution {
+private:
+    void findcomb( int ind,int target,vector<int>&arr,vector<vector<int>>
+    &ans,vector<int>&comb){
+        if(ind>arr.size()-1){
+            if(target==0){
+                ans.push_back(comb);
+            }
+            return ;
+        }
+        if(arr[ind]<=target){
+            comb.push_back(arr[ind]);
+            findcomb(ind,target-arr[ind],arr,ans,comb);
+            comb.pop_back();
+        }
+        findcomb(ind+1,target,arr,ans,comb);
+    }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> res;
-        vector<int> comb;
-        makeCombination(candidates, target, 0, comb, 0, res);
-        return res;        
+        vector<vector<int>>ans;
+        vector<int>comb;
+        findcomb(0,target,candidates,ans,comb);
+        return ans;
+        
     }
-
-private:
-    void makeCombination(std::vector<int>& candidates, int target, int idx, vector<int>& comb, int total, vector<vector<int>>& res) {
-        if (total == target) {
-            res.push_back(comb);
-            return;
-        }
-
-        if (total > target || idx >= candidates.size()) {
-            return;
-        }
-
-        comb.push_back(candidates[idx]);
-        makeCombination(candidates, target, idx, comb, total + candidates[idx], res);
-        comb.pop_back();
-        makeCombination(candidates, target, idx + 1, comb, total, res);
-    }    
 };
