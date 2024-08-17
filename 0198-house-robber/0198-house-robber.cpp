34abcbd76429e45ec978e559db034fab7f927220
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2024-08-18 00:49:21 +0530

    Time: 0 ms (100%), Space: 9.6 MB (30.23%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,39 @@
 class Solution {
 public:
     int solve(vector<int>& nums , int n , vector<int>&dp){
-        if(n==0) return nums[n];
-        if(n<0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        int take = solve(nums,n-2,dp) + nums[n];
-        int nottake =solve(nums,n-1,dp);
-        return dp[n]=max(take , nottake);
+        dp[0]=nums[0];
+        for(int i=1;i<=n;i++){
+        int take = nums[i];
+        if(i>1) { take+=dp[i-2];}
+        int nottake =dp[i-1];
+        dp[i]=max(take , nottake);}
+        return dp[n];
     }
     int rob(vector<int>& nums) {
         int n= nums.size();
-        vector<int>dp(n,-1);
+        vector<int>dp(n,0);
         return solve(nums,n-1,dp);
     }
 };
+
+// class Solution {
+// public: 
+//     int solve(int n,vector<int>&nums,vector<int>&dp){
+//        dp[0]=nums[0];
+//        for(int i=1;i<n;i++){
+//         int sel=nums[i];
+//         if(i>1){
+//             sel+=dp[i-2];
+//         }
+//         int notsel=dp[i-1];
+       
+//        dp[i]=max(sel,notsel);
+//        }
+//        return dp[n-1];
+// }
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//        vector<int>dp(n,0);
+//         return solve(n,nums,dp);
+//     }
+// };
