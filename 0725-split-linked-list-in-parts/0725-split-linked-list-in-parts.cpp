1d581fe91ba48a69fb25f5008aaac24de6ec48b6
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2024-09-22 01:21:25 +0530

    Time: 4 ms (72.31%), Space: 13.9 MB (15.43%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
@@ -10,34 +10,54 @@
  */
 class Solution {
 public:
-    vector<ListNode*> splitListToParts(ListNode* head, int k) 
-    {
-        vector<ListNode*> node;
-        ListNode* curr = head;
-        node.push_back(curr);
-        int n=0;
-        while (curr) {     
-            curr=curr->next;
-            node.push_back(curr);
-            n++;
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        vector<ListNode*> ans(k);
+
+        // get total size of linked list
+        int size = 0;
+        ListNode* current = head;
+        while (current != nullptr) {
+            size++;
+            current = current->next;
         }
 
-        auto [q, r]=div(n, k);
-       
-        vector<int> iLen(k, q);
-        fill(iLen.begin(), iLen.begin()+r, q+1);
-        vector<ListNode*> ans(k, {});
-        int beginIdx=0, endIdx;
-        
-        //Consider only the heads & tails for ans[i]
-        for (int i=0; i<k; i++) {
-            beginIdx=(i==0) ? 0:beginIdx+iLen[i-1];
-            endIdx=(iLen[i]==0)?beginIdx:beginIdx+iLen[i]-1;
-            ans[i]=node[beginIdx];
-            ListNode* last = node[endIdx];
-            if (last) 
-                last->next = NULL; // Remove the link
-        }       
+        // minimum size for the k parts
+        int splitSize = size / k;
+
+        // Remaining nodes after splitting the k parts evenly.
+        // These will be distributed to the first (size % k) nodes
+        int numRemainingParts = size % k;
+
+        current = head;
+        ListNode* prev = current;
+        for (int i = 0; i < k; i++) {
+            // create the i-th part
+            ListNode* newPart = current;
+            // calculate size of i-th part
+            int currentSize = splitSize;
+            if (numRemainingParts > 0) {
+                numRemainingParts--;
+                currentSize++;
+            }
+
+            // traverse to end of new part
+            int j = 0;
+            while (j < currentSize) {
+                prev = current;
+                if (current != nullptr) {
+                    current = current->next;
+                }
+                j++;
+            }
+
+            // cut off the rest of linked list
+            if (prev != nullptr) {
+                prev->next = nullptr;
+            }
+
+            ans[i] = newPart;
+        }
+
         return ans;
     }
 };
