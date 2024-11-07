cfeaf37ae723e66b3c5cf9ca6f5ee82bb03a404c
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2024-11-07 10:52:27 +0530

    Time: 2 ms (23.65%), Space: 8 MB (89.25%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,27 +1,56 @@
+// class Solution {
+// public:
+//     string removeOuterParentheses(string s) {
+//         stack<char>st;
+//         string ans="";
+//         int n= s.size();
+//         for(int i=0;i<n;i++){
+//             if(st.empty()){
+//                 st.push(s[i]);
+//             }
+//             else{
+//                 if(s[i]=='('){
+//                     st.push(s[i]);
+//                     ans+=s[i];
+//                 }
+//                 else{
+//                     st.pop();
+//                     if(!st.empty()){
+//                         ans+=s[i];
+//                     }
+//                 }
+//             }
+//         }
+//         return ans;
+        
+//     }
+// };
+
+#include <string>
+using namespace std;
+
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        stack<char>st;
-        string ans="";
-        int n= s.size();
-        for(int i=0;i<n;i++){
-            if(st.empty()){
-                st.push(s[i]);
-            }
-            else{
-                if(s[i]=='('){
-                    st.push(s[i]);
-                    ans+=s[i];
+        string ans = "";
+        int depth = 0;  // Track the depth of nested parentheses
+
+        for (char c : s) {
+            if (c == '(') {
+                // Only add to result if it's not an outermost '('
+                if (depth > 0) {
+                    ans += c;
                 }
-                else{
-                    st.pop();
-                    if(!st.empty()){
-                        ans+=s[i];
-                    }
+                depth++;
+            } else {
+                depth--;
+                // Only add to result if it's not an outermost ')'
+                if (depth > 0) {
+                    ans += c;
                 }
             }
         }
+
         return ans;
-        
     }
 };
