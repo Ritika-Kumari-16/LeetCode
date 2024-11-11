401ce1a3b892e46e40d420e71f111b71e4eb89cf
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2024-11-11 12:56:26 +0530

    Time: 66 ms (5.14%), Space: 125.2 MB (6.4%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        string out = "1";
-
-        for (int i = 0; i < n - 1; i++) {
-            string str = "";
-            char prevChar = out[0];
-            int count = 1;
-
-            for (int j = 1; j < out.length(); j++) {
-                if (out[j] == prevChar) {
-                    count++;
-                } else {
-                    str += to_string(count) + prevChar;
-                    prevChar = out[j];
-                    count = 1;
-                }
+        if(n==1) return "1";
+        string say = countAndSay(n-1);
+        string ans="";
+        int i=0;
+        while(i<say.size()){
+            char ch=say[i];
+            int count=0;
+            while(i<say.size()){
+                if(say[i]!=ch) break;
+                count++;
+                i++;
             }
-
-            str += to_string(count) + prevChar;
-            out = str;
+            ans= ans + to_string(count)+string (1,ch);
         }
-
-        return out;
+        return ans;
     }
 };
