4adb3821c8b63f54773ebca61030cb4b853a23fa
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2024-12-12 20:42:15 +0530

    Time: 113 ms (73.68%), Space: 9.3 MB (43.17%) - LeetHub

--- 2360-substring-with-largest-variance/2360-substring-with-largest-variance.cpp
+++ 2360-substring-with-largest-variance/2360-substring-with-largest-variance.cpp
@@ -1,50 +1,46 @@
 class Solution {
 public:
     int largestVariance(string s) {
-        vector<int> counter(26, 0);
-        for (char ch : s) {
-            counter[ch - 'a']++;
+        int arr[26]={0};
+        for(int i=0;i<s.size();i++){
+            arr[s[i]-'a']++;
+
         }
-        int globalMax = 0;
-        
-        for (int i = 0; i < 26; i++) {
-            for (int j = 0; j < 26; j++) {
-                // major and minor cannot be the same, and both must appear in s.
-                if (i == j || counter[i] == 0 || counter[j] == 0) {
+        int result=0;
+        for(int i=0;i<26;i++){
+            if(arr[i]==0) continue;
+            for(int j=0;j<26;j++){
+                if( arr[j]==0){
                     continue;
                 }
-                
-                // Find the maximum variance of major - minor.        
-                char major = 'a' + i;
-                char minor = 'a' + j;
-                int majorCount = 0;
-                int minorCount = 0;
-                
-                // The remaining minor in the rest of s.
-                int restMinor = counter[j];
-                
-                for (char ch : s) {    
-                    if (ch == major) {
-                        majorCount++;
+                int firstcount=0;
+                int secondcount=0;
+                bool visitedsec=false;
+                for(char &ch : s){
+                    if(i==ch-'a'){
+                        firstcount++;
                     }
-                    if (ch == minor) {
-                        minorCount++;
-                        restMinor--;
+                    if(j==ch-'a'){
+                        secondcount++;
                     }
-                    
-                    // Only update the variance (local_max) if there is at least one minor.
-                    if (minorCount > 0)
-                        globalMax = max(globalMax, majorCount - minorCount);
-                    
-                    // We can discard the previous string if there is at least one remaining minor
-                    if (majorCount < minorCount && restMinor > 0) {
-                        majorCount = 0;
-                        minorCount = 0;
+                    if(secondcount>0){
+                        result=max(result,firstcount-secondcount);
+
                     }
+                    else{
+                        if(visitedsec==true){
+                            result=max(result,firstcount-1);
+                        }
+                    }
+                    if(secondcount > firstcount){
+                        firstcount=0;
+                        secondcount=0;
+                        visitedsec=true;
+                    }
+
                 }
             }
         }
-        
-        return globalMax;
+        return result;
     }
 };
