7dc2e0b6ab675171ef9929137575a16df2cd8df8
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2024-12-17 16:14:14 +0530

    Time: 1 ms (86.44%), Space: 12.8 MB (80.99%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,34 +1,36 @@
-void backtrack(int n, int leftCount, int rightCount, vector<char>& output,
-               vector<string>& result) {
-    // Base case where count of left and right braces is "n"
-    if (leftCount >= n && rightCount >= n) {
-        // Join the array elements into a string without any separators.
-        string outputStr(output.begin(), output.end());
-        result.push_back(outputStr);
-    }
-
-    // Case where we can still append left braces
-    if (leftCount < n) {
-        output.push_back('(');
-        backtrack(n, leftCount + 1, rightCount, output, result);
-        output.pop_back();
-    }
-
-    // Case where we append right braces if the current count of right braces is
-    // less than the count of left braces
-    if (rightCount < leftCount) {
-        output.push_back(')');
-        backtrack(n, leftCount, rightCount + 1, output, result);
-        output.pop_back();
-    }
-}
-
 class Solution {
 public:
+    void solve( int n, string &temp, vector<string> &result , int open, int close ){
+        if(temp.size()==n*2 && close==open){
+            result.push_back(temp);
+            return ;
+        }
+        //taking (
+        if(open <n){
+            temp.push_back('(');
+            open++;
+            solve(n,temp,result,open,close);
+            temp.pop_back();
+            open--;
+        }
+        
+        //taking )
+        if(close <open){
+            temp.push_back(')');
+            close++;
+            solve(n,temp,result,open,close);
+            temp.pop_back();
+            close--;
+        }
+
+    }
     vector<string> generateParenthesis(int n) {
-        vector<string> result;
-        vector<char> output;
-        backtrack(n, 0, 0, output, result);
+        vector<string>result;
+        string temp="";
+        int open=0;
+        int close=0;
+        solve(n,temp,result,open , close);
         return result;
+        
     }
 };
