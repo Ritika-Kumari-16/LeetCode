53818d846cf91578d63d13787ae704c06f5a881e
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-01-01 13:08:31 +0530

    Time: 0 ms (100%), Space: 8.6 MB (52.6%) - LeetHub

---
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxScore(string s) {
+        int n=s.size();
+        int score= INT_MIN;
+        int ones=0;
+        int zeros=0;
+        for(int i=0;i<=n-2;i++){
+            if(s[i]=='0'){
+                zeros++;
+            }
+            else {ones++ ;}
+            score=max(score,zeros-ones);
+        }
+        if(s[n-1]=='1') ones++;
+        return score+ones;
+        
+    }
+};
