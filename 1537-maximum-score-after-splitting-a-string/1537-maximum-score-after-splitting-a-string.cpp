4a530c8f92ab4afc701dd949a8b99d8eb1d92b3f
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-01-01 13:17:22 +0530

    Time: 0 ms (100%), Space: 8.9 MB (23.35%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -1,19 +1,47 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int n=s.size();
-        int score= INT_MIN;
-        int ones=0;
-        int zeros=0;
-        for(int i=0;i<=n-2;i++){
-            if(s[i]=='0'){
-                zeros++;
+        int n= s.size();
+        int score=INT_MIN;
+        int left_zeros=0;
+        int left_ones=0;
+        int right_ones=0;
+        int total=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='1'){
+                total++;
             }
-            else {ones++ ;}
-            score=max(score,zeros-ones);
         }
-        if(s[n-1]=='1') ones++;
-        return score+ones;
+        for(int i=0;i<=n-2;i++){
+            if(s[i]=='1') left_ones++;
+            else{
+                left_zeros++;
+            }
+            right_ones=total-left_ones;
+            score=max(score,left_zeros+right_ones);
+        }
+        return score;
         
     }
 };
+
+
+// class Solution {
+// public:
+//     int maxScore(string s) {
+//         int n=s.size();
+//         int score= INT_MIN;
+//         int ones=0;
+//         int zeros=0;
+//         for(int i=0;i<=n-2;i++){
+//             if(s[i]=='0'){
+//                 zeros++;
+//             }
+//             else {ones++ ;}
+//             score=max(score,zeros-ones);
+//         }
+//         if(s[n-1]=='1') ones++;
+//         return score+ones;
+        
+//     }
+// };
