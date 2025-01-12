47e0606c736d03e0fc19a8bafd1abbd469a2e1ff
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-01-12 23:49:07 +0530

    Time: 7 ms (95.69%), Space: 30.1 MB (53.05%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,36 +1,76 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
-
-        int n = s.size();
-        if(n%2==1) return false;
-        stack<int> open;
-        stack<int>openclose;
-            for(int i=0;i<n;i++){
-
-                if(locked[i]=='0'){
-                    openclose.push(i);
-                }
-                else if(s[i]=='('){
-                        open.push(i);
-                    }
-                    else if(s[i]==')'){
-                        if(!open.empty()){
-                            open.pop();
-                        }
-                        else if(!openclose.empty()){
-                                openclose.pop();
-                            }
-                            else{
-                                return false;
-                            }
-                        }
-                }
-    while(!open.empty() && !openclose.empty() && open.top()<openclose.top()){
-            open.pop();
-            openclose.pop();
-
+        int n = s.length();
+        if (n % 2 != 0) {
+            return false; // Odd length cannot form valid parentheses
         }
-    return open.empty();
+
+        // Left-to-right pass: Ensure there are enough open brackets
+        int openCount = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(' || locked[i] == '0') {
+                openCount++;
+            } else { // s[i] == ')' and locked[i] == '1'
+                openCount--;
+            }
+            if (openCount < 0) {
+                return false; // Too many ')' encountered
+            }
+        }
+
+        // Right-to-left pass: Ensure there are enough close brackets
+        int closeCount = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == ')' || locked[i] == '0') {
+                closeCount++;
+            } else { // s[i] == '(' and locked[i] == '1'
+                closeCount--;
+            }
+            if (closeCount < 0) {
+                return false; // Too many '(' encountered
+            }
+        }
+
+        return true;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool canBeValid(string s, string locked) {
+
+//         int n = s.size();
+//         if(n%2==1) return false;
+//         stack<int> open;
+//         stack<int>openclose;
+//             for(int i=0;i<n;i++){
+
+//                 if(locked[i]=='0'){
+//                     openclose.push(i);
+//                 }
+//                 else if(s[i]=='('){
+//                         open.push(i);
+//                     }
+//                     else if(s[i]==')'){
+//                         if(!open.empty()){
+//                             open.pop();
+//                         }
+//                         else if(!openclose.empty()){
+//                                 openclose.pop();
+//                             }
+//                             else{
+//                                 return false;
+//                             }
+//                         }
+//                 }
+//     while(!open.empty() && !openclose.empty() && open.top()<openclose.top()){
+//             open.pop();
+//             openclose.pop();
+
+//         }
+//     return open.empty();
+//     }
+// };
