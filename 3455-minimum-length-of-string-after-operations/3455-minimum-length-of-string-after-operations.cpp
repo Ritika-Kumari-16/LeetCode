4230af1e8224a738a74861ec09bc161d5709e3c9
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-01-14 00:19:04 +0530

    Time: 3 ms (98.79%), Space: 31.9 MB (86.69%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        // Step 1: Initialize a frequency array to count occurrences of each
-        // character
-        vector<int> charFrequency(26, 0);
-        int totalLength = 0;
-
-        // Step 2: Count the frequency of each character in the string
+        vector<int> Freq(26, 0);
+        int totalLen = 0;
         for (char currentChar : s) {
-            charFrequency[currentChar - 'a']++;
+            Freq[currentChar - 'a']++;
         }
-
-        // Step 3: Calculate the total length after deletions count
-        for (int frequency : charFrequency) {
-            if (frequency == 0) continue;  // Skip characters that don't appear
+        for (int frequency : Freq) {
+            if (frequency == 0) continue;
             if (frequency % 2 == 0) {
-                totalLength += 2;  // If frequency is even, add 2
+                totalLen += 2;  // If frequency is even, add 2
             } else {
-                totalLength += 1;  // If frequency is odd, add 1
+                totalLen+= 1;  // If frequency is odd, add 1
             }
         }
 
-        // Step 4: Return the minimum length after deletions count
-        return totalLength;
+        return totalLen;
     }
 };
