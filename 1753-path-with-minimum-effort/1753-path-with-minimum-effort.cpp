fa0179595780ec1648188bcfb048bb7af05c59ab
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-01-30 17:41:54 +0530

    Time: 51 ms (65.59%), Space: 24.5 MB (53.71%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,37 +1,36 @@
-
 class Solution {
-    using p = tuple<int, int, int>;
-
 public:
-    int minimumEffortPath(vector<vector<int>>& A) {
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int n= heights.size();
+        int m= heights[0].size();
+        vector<vector<int>>distance(n,vector<int>(m,1e9));
+        distance[0][0]=0;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        pq.push({0,{0,0}});
+        int row[]={-1,0,0,1};
+        int col[]={0,-1,1,0};
+        while(!pq.empty()){
+            auto it = pq.top();
+            int dist= it.first;
+            int r= it.second.first;
+            int c=it.second.second;
+            if(r==n-1 && c==m-1) return dist;
+            pq.pop();
+            for(int i=0;i<4;i++){
+                int newr=r+row[i];
+                int newc=c+col[i];
 
-        int m = A.size(), n = A[0].size();
-        priority_queue<p, vector<p>, greater<p>> q;
-
-        vector<vector<int>> Eff(m, vector<int>(n, INT_MAX));
-        q.push({0, 0, 0}); Eff[0][0]=0;
-
-        vector<pair<int, int>> dirn = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
-        while (!q.empty()) {
-            auto [eff, i, j] = q.top();
-            q.pop();
-
-            if (eff > Eff[i][j])
-                continue;
-
-            for (auto& dir : dirn) {
-                int x = i + dir.first, y = j + dir.second;
-
-                if (x >= 0 && x < m && y >= 0 && y < n) {
-                    int diff = abs(A[x][y] - A[i][j]), effort = max(eff, diff);
-                    
-                    if(effort<Eff[x][y])
-                    Eff[x][y] = effort, q.push({effort, x, y});
+                if(newr>=0 && newr<n && newc>=0 && newc<m){
+                    int diff= abs(heights[r][c]-heights[newr][newc]);
+                    int newdis=max(diff,dist);
+                    if(newdis < distance[newr][newc]){
+                        distance[newr][newc]=newdis;
+                        pq.push({newdis,{newr,newc}});
+                    }
                 }
             }
-        }
 
-        return Eff[m - 1][n - 1];
+        }
+        return distance[n-1][m-1];
     }
 };
