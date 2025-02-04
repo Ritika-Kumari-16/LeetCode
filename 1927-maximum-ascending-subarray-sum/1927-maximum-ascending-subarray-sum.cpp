89626969eda4026b60f028c861e8d31cebc6f0bc
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-02-04 18:53:33 +0530

    Time: 0 ms (100%), Space: 11 MB (77.09%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.cpp
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
     int maxAscendingSum(vector<int>& nums) {
-        int maxsum=0;
+        int maxsum=nums[0];
         int n = nums.size();
-        for( int i=0;i<n;i++){
-            int sum=nums[i];
-            for(int j=i+1;j<n;j++){
-                if(nums[j-1]<nums[j]){
-                    sum+=nums[j];
-                    // maxsum=max(maxsum,sum);
-                }
-                else{
-                    break;
-                }
+        int sum=nums[0];
+        for(int i=1;i<n;i++){
+            if(nums[i-1]<nums[i]){
+                sum+=nums[i];
+                maxsum=max(maxsum,sum);
             }
-            maxsum=max(maxsum,sum);
+            else{
+                sum=nums[i];
+            }
         }
+
+        // for( int i=0;i<n;i++){
+        //     int sum=nums[i];
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[j-1]<nums[j]){
+        //             sum+=nums[j];
+        //             // maxsum=max(maxsum,sum);
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        //     maxsum=max(maxsum,sum);
+        // }
         return maxsum;
     }
 };
