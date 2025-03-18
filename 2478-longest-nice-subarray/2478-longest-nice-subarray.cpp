9f885e1c293db55746230420bdbb7b3045baf057
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-03-19 00:54:28 +0530

    Time: 0 ms (100%), Space: 61.1 MB (28.44%) - LeetHub

---
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        int l=0;
+        int r=0;
+        int maxlen=1;
+        int mask=0;
+        int n= nums.size();
+        while(r<n){
+            while((mask & nums[r])!=0){
+                mask=(mask^ nums[l]);
+                l++;
+            }
+            maxlen=max(maxlen ,r-l+1);
+            mask=(mask | nums[r]);
+            r++;
+        }
+        return maxlen;
+        
+    }
+};
