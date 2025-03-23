3aba475dcf329f312700e1b3e21e6a819ab891ff
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-03-23 22:25:27 +0530

    Time: 3 ms (91.44%), Space: 35 MB (88.45%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -1,42 +1,41 @@
 class Solution {
 public:
-    const long long mod=1e9+7;
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<pair<int,int>>adj[n];
-        for(auto it:roads){
-            adj[it[0]].push_back({it[1],it[2]});
-            adj[it[1]].push_back({it[0],it[2]});
+        vector<vector<pair<int, int>>> graph(n);
+        for (const auto& road : roads) {
+            int u = road[0], v = road[1], time = road[2];
+            graph[u].emplace_back(v, time);
+            graph[v].emplace_back(u, time);
         }
-        vector<long long>distance(n,LLONG_MAX);
-        vector<long long>ways(n,0);
-        distance[0]=0;
-        ways[0]=1;
-        priority_queue<pair<long long ,long long>,vector<pair<long long , long long>>,greater<pair<long long , long long>>>pq;
-        pq.push({0,0});
-        while(!pq.empty()){
-            long long dist=pq.top().first;
-            long long node=pq.top().second;
+
+        vector<long long> dist(n, LLONG_MAX);
+        vector<int> ways(n, 0);
+
+        dist[0] = 0;
+        ways[0] = 1;
+
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
+        pq.emplace(0, 0);
+
+        const int MOD = 1e9 + 7;
+
+        while (!pq.empty()) {
+            auto [d, node] = pq.top();
             pq.pop();
-            for(auto it :adj[node]){
-                long long pathdis=it.second;
-                long long adjnode=it.first;
-                if(dist+pathdis<distance[adjnode]){
-                    distance[adjnode]=dist+pathdis;
-                    ways[adjnode]=ways[node];
-                    pq.push({dist+pathdis,adjnode});
-                }
-                else if(dist+pathdis==distance[adjnode]){
-                    ways[adjnode]+=ways[node]%mod;
+
+            if (d > dist[node]) continue;
+
+            for (const auto& [neighbor, time] : graph[node]) {
+                if (dist[node] + time < dist[neighbor]) {
+                    dist[neighbor] = dist[node] + time;
+                    ways[neighbor] = ways[node];
+                    pq.emplace(dist[neighbor], neighbor);
+                } else if (dist[node] + time == dist[neighbor]) {
+                    ways[neighbor] = (ways[neighbor] + ways[node]) % MOD;
                 }
             }
         }
-        if(distance[n-1]==1e9){
-            return -1;
-        }
-        else{
-            return ways[n-1]%mod;
-        }
-    // return ways[n-1]%mod ;
-        
+
+        return ways[n - 1];
     }
 };
