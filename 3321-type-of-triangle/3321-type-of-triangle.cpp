dfddb959ed17673b18cd1b104f122de9569a9dbc
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-05-19 12:37:47 +0530

    Time: 0 ms (100%), Space: 22.8 MB (88.05%) - LeetHub

--- 3321-type-of-triangle/3321-type-of-triangle.cpp
+++ 3321-type-of-triangle/3321-type-of-triangle.cpp
@@ -1,38 +1,13 @@
 class Solution {
 public:
-    bool scalenetri(vector<int>&nums){
-        if(nums[0]+nums[1]<=nums[2]) return false;
-        if(nums[1]+nums[2]<=nums[0]) return false;
-        if(nums[0]+nums[2]<=nums[1]) return false;
-        return true;
-    }
     string triangleType(vector<int>& nums) {
-        string ans;
-        int same=1;
-        sort(nums.begin(),nums.end());
-        if(scalenetri(nums)==false){
-            return "none";
+        int a = nums[0], b = nums[1], c = nums[2];
+
+        if(a + b > c && b + c > a && a + c > b) {
+            if(a == b && b == c && c == a) return "equilateral";
+            else if((a == b) || (b == c) || (c == a)) return "isosceles";
+            return "scalene";
         }
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1]){
-                same++;
-            }
-            
-        }
-        if(same==2){
-            ans="isosceles";
-        }
-        else if(same==3){
-            ans="equilateral";
-        }
-        else{
-            if(scalenetri(nums)){
-                ans="scalene";
-            }
-            else{
-                ans="none";
-            }
-        }
-        return ans;
+        return "none";
     }
 };
