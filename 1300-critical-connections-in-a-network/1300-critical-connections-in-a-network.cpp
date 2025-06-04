b98285d83618fd74696c8a91203999248edaf7e7
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-06-04 22:45:32 +0530

    Time: 136 ms (90.17%), Space: 170.1 MB (77.48%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
@@ -1,41 +1,45 @@
 class Solution {
 public:
-    vector<int> lowlink;
-    vector<int> tin;
-    vector<vector<int>> res;
-    vector<bool> vis;
-    int timer = 0;
-    // return lowlink of children
-    int dfs(int p, int i, unordered_map<int, vector<int>>& g) {
-        if (vis[i]) return lowlink[i];
+    int timestamp = 1;
 
-        timer++;
-        vis[i] = 1; // first time visiting this node
-        lowlink[i] = tin[i] = timer;
+    void dfs(int node, int parent, vector<vector<int>>& adjlist, vector<int>& time,
+             vector<int>& low, vector<int>& visited, vector<vector<int>>& bridges) {
 
-        for (auto nb: g[i]) {
-            if (nb == p) continue;
-            lowlink[i] = std::min(lowlink[i], dfs(i, nb, g));
+        visited[node] = 1;
+        time[node] = low[node] = timestamp++;
+
+        for (auto adj : adjlist[node]) {
+            if (adj == parent) continue;
+
+            if (!visited[adj]) {
+                dfs(adj, node, adjlist, time, low, visited, bridges);
+
+                // After returning, update low time
+                low[node] = min(low[node], low[adj]);
+
+                // Bridge condition
+                if (low[adj] > time[node]) {
+                    bridges.push_back({node, adj});
+                }
+            } else {
+                // Back edge
+                low[node] = min(low[node], time[adj]);
+            }
         }
-
-        if (p != -1 && lowlink[i] == tin[i]) res.push_back({p, i});
-
-        return lowlink[i];
     }
 
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        lowlink = vector<int>(n, INT_MAX);
-        tin = vector<int>(n, INT_MAX);
-        vis = vector<bool>(n, 0);
-
-        unordered_map<int, vector<int>> g;
-        for (auto& c: connections) {
-            g[c[0]].push_back(c[1]);
-            g[c[1]].push_back(c[0]);
+        vector<vector<int>> adjlist(n);
+        for (auto& conn : connections) {
+            int u = conn[0], v = conn[1];
+            adjlist[u].push_back(v);
+            adjlist[v].push_back(u);
         }
 
-        dfs(-1, 0, g);
+        vector<int> time(n, -1), low(n, -1), visited(n, 0);
+        vector<vector<int>> bridges;
 
-        return res;
+        dfs(0, -1, adjlist, time, low, visited, bridges);
+        return bridges;
     }
 };
