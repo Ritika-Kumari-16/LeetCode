99df2155981c565836297461222a08675b5b1e48
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-06-04 22:51:17 +0530

    Time: 138 ms (88.94%), Space: 170 MB (80.24%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
-    int timestamp = 1;
-
-    void dfs(int node, int parent, vector<vector<int>>& adjlist, vector<int>& time,
-             vector<int>& low, vector<int>& visited, vector<vector<int>>& bridges) {
-
-        visited[node] = 1;
-        time[node] = low[node] = timestamp++;
-
-        for (auto adj : adjlist[node]) {
-            if (adj == parent) continue;
-
-            if (!visited[adj]) {
-                dfs(adj, node, adjlist, time, low, visited, bridges);
-
-                // After returning, update low time
-                low[node] = min(low[node], low[adj]);
-
-                // Bridge condition
-                if (low[adj] > time[node]) {
-                    bridges.push_back({node, adj});
+    int timestamp=1;
+    void dfs( int node ,int parent ,vector<vector<int>>&adjlist,vector<int>&time , vector<int>&minitime , vector<int>&visited , vector<vector<int>>&criticaledges ){
+        visited[node]=1;
+        time[node]=timestamp;
+        minitime[node]=timestamp;
+        timestamp++;
+        for(auto adjnode : adjlist[node]){
+            if(adjnode==parent) continue;
+            if(visited[adjnode]==0){
+                dfs(adjnode ,node , adjlist , time , minitime , visited , criticaledges);
+                minitime[node]=min(minitime[node],minitime[adjnode]);
+                if(minitime[adjnode]>time[node]){
+                criticaledges.push_back({node,adjnode});
                 }
-            } else {
-                // Back edge
-                low[node] = min(low[node], time[adj]);
             }
-        }
-    }
+            else{
+                minitime[node]=min(minitime[node],time[adjnode]);
+            }
 
+        }
+
+
+    }
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        vector<vector<int>> adjlist(n);
-        for (auto& conn : connections) {
-            int u = conn[0], v = conn[1];
+        //make the graph
+        vector<vector<int>>adjlist(n);
+        for(auto &it : connections){
+            int u = it[0];
+            int v= it[1];
             adjlist[u].push_back(v);
             adjlist[v].push_back(u);
         }
-
-        vector<int> time(n, -1), low(n, -1), visited(n, 0);
-        vector<vector<int>> bridges;
-
-        dfs(0, -1, adjlist, time, low, visited, bridges);
-        return bridges;
+        vector<int>time(n,-1);
+        vector<int>minitime(n,-1);
+        vector<int>visited(n,0);
+        vector<vector<int>>criticaledges;
+        dfs(0,-1,adjlist,time,minitime,visited,criticaledges);
+        return criticaledges;
     }
 };
