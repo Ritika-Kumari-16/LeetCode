f7130f5dd6cd0916fda1a012c0a29fb093fcfc66
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-06-05 22:09:42 +0530

    Time: 16 ms (53.81%), Space: 101.9 MB (71.05%) - LeetHub

--- 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    void dfs(int current, int distance, const vector<int>& edges, vector<int>& distances) {
-        while (current != -1 && distances[current] == -1) {
-            distances[current] = distance++;
-            current = edges[current];
+    void dfs(vector<int>& edges , int start , vector<int>& dist1){
+        int dist=0;
+        while(start != -1 && dist1[start]==-1){
+            dist1[start]=dist;
+            dist++;
+            start=edges[start];
         }
     }
-
-    int closestMeetingNode(vector<int>& edges, int start1, int start2) {
-        int res = -1, Min_Of_Max = INT_MAX, n = edges.size();
-        vector<int> dist1(n, -1), dist2(n, -1);
-        dfs(start1, 0, edges, dist1);
-        dfs(start2, 0, edges, dist2);
-
-        for (int i = 0; i < n; i++) {
-            if (dist1[i] >= 0 && dist2[i] >= 0) {
-                int maxDist = max(dist1[i], dist2[i]);
-                if (maxDist < Min_Of_Max) {
-                    Min_Of_Max = maxDist;
-                    res = i;
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        int n = edges.size();
+        vector<int>dist1(n,-1);
+        vector<int>dist2(n,-1);
+        dfs(edges,node1,dist1);
+        dfs(edges,node2,dist2);
+        int maxdis=INT_MAX;
+        int idx=-1;
+        for(int i=0;i<n;i++){
+            if(dist1[i]>=0 && dist2[i]>=0){
+                int dis=max(dist1[i],dist2[i]);
+                if(dis<maxdis){
+                    maxdis=dis;
+                    idx=i;
                 }
             }
         }
-        return res;
+        return idx;
     }
 };
