ec96fb32d6de5bfd5c932ccfd6985f6ab49e9c56
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-06-22 17:23:37 +0530

    Time: 0 ms (100%), Space: 9.5 MB (37.54%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        int left =0, top=0;
-        int bottom = n-1;
-        int right=n-1;
-        int val=1;
         vector<vector<int>>matrix(n,vector<int>(n,0));
-        while(top<=bottom && left<=right){
-            for(int i=left;i<=right;i++){
-                matrix[top][i]=val;
-                val++;
+        int count=1;
+        int top=0;
+        int left=0;
+        int bottom=n-1;
+        int right=n-1;
+        while(top<=bottom && left<=right){  
+            for(int i= left ;i<=right;i++){
+                matrix[top][i]=count;
+                count++;
             }
             top++;
-            for(int i=top;i<=bottom;i++){
-                matrix[i][right]=val;
-                val++;
+            for(int i=top;i<=bottom;i++ ){
+                matrix[i][right]=count;
+                count++;
             }
             right--;
             if(top<=bottom){
-                for(int i=right;i>=left;i--){
-                    matrix[bottom][i]=val;
-                    val++;
+                for(int i= right;i>=left;i--){
+                    matrix[bottom][i]=count;
+                    count++;
                 }
                 bottom--;
             }
             if(left<=right){
-                for(int i=bottom;i>=top;i--){
-                    matrix[i][left]=val;
-                    val++;
+                for(int i=bottom ; i>=top;i--){
+                    matrix[i][left]=count;
+                    count++;
+
                 }
                 left++;
-        }
+            }
         }
         return matrix;
         
