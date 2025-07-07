38d82d80ad798389fa41a912039be8893e34c842
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-07 10:02:57 +0530

    Time: 6 ms (78.05%), Space: 23.1 MB (12.66%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,31 +1,29 @@
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-long long fun(vector<int>&piles , int hourly){
-    long long reqtime=0;
-    for(int i=0;i<piles.size();i++){
-    reqtime+=ceil((double)piles[i]/ hourly);
-    }
-    return reqtime;
-}
 class Solution {
 public:
+long long solve(vector<int>&piles , int bananas){
+    long long totalhrs=0;
+    for(int i=0;i<piles.size();i++){
+        totalhrs+=ceil(double(piles[i])/ double(bananas));
+    }
+    return totalhrs;
+}
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n=piles.size();
-        int mid;
-        int low= 1;
-        int high=*max_element(piles.begin(),piles.end());
-        int ans =high;
+        int n= piles.size();
+        if(h<n) return -1;
+        int low=1;
+        int high= *max_element(piles.begin(),piles.end());
+        int ans=high;
         while(low<=high){
-            mid=low+(high-low)/2;
-            long long totalhrs=fun(piles,mid);
-            if(totalhrs>h){
-                low=mid+1;
-            }
-            else{
+            int mid= low+(high-low)/2;
+            long long timetaken =solve(piles,mid);
+            if(timetaken<=h){
                 ans=mid;
                 high=mid-1;
             }
+            else{
+                low=mid+1;
+            }
+
         }
         return ans;
     }
