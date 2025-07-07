4fd8247823e08845690973efb05c61a3f18e2d8a
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-07 22:52:47 +0530

    Time: 0 ms (100%), Space: 50.5 MB (16.99%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
@@ -1,44 +1,38 @@
-int maxeleincol(vector<vector<int>>&arr,int mid,int n){
-    int index=-1;
-    int maxval=-1;
-    for(int i=0;i<n;i++){
-        if(arr[i][mid]>maxval){
-            maxval=arr[i][mid];
-            index=i;
-        }
-    }
-    return index;
-}
 class Solution {
 public:
-    vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m=mat[0].size();
-        int low=0 , high=m-1;
-        while(low<=high){
-            int mid= (low+high)/2;
-            int indmx=maxeleincol(mat,mid,n);
-            int left,right;
-            if(mid-1>=0){
-                left=mat[indmx][mid-1];
-                }
-            else{
-                left=-1;
+    int maxelincol(vector<vector<int>>& mat , int col){
+        int maxel=-1;
+        int idx=-1;
+        for(int i=0;i<mat.size();i++){
+            if(mat[i][col]>maxel){
+                maxel=mat[i][col];
+                idx=i;
             }
-            if(mid+1<=m-1){
-                right=mat[indmx][mid+1];
-            }
-            else {
-                right=-1;
-            }
-            if(left<mat[indmx][mid]&&mat[indmx][mid]>right){
-                return {indmx,mid};
-            }
-            else if(left>mat[indmx][mid]){high=mid-1;}
-            else{low=mid+1;}
-        
+        }
+        return idx;
     }
-    
-    return {-1,-1};
+    vector<int> findPeakGrid(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int low=0;
+        int high=m-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int maxelindex=maxelincol(matrix,mid);
+            int left=-1,right=-1;
+            if(mid-1>=0) left=matrix[maxelindex][mid-1];
+            if(mid+1<m) right=matrix[maxelindex][mid+1];
+            if(left<matrix[maxelindex][mid] && matrix[maxelindex][mid] > right){
+                return {maxelindex,mid};
+            }
+            else if(matrix[maxelindex][mid]<left){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return {-1,-1};
+        
     }
 };
