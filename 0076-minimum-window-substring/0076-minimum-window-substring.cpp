fedb443da0e7b14792983f037d006b6ca4a06f2a
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-08 10:26:09 +0530

    Time: 4 ms (76.43%), Space: 11.5 MB (44.48%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int l=0;
-        int r=0;
-        int n= s.size();
+        unordered_map<char,int>mpp;
+        int n = s.size();
         int m= t.size();
-        int minlen= INT_MAX;
-        int cnt=0;
-        int sind=-1;
-        vector<int>hash(256,0);
-        for(int i=0;i<m;i++){
-            hash[t[i]]++;
+        for(auto it :t){
+            mpp[it]++;
         }
+        int l=0 , r=0;
+        int count=0;
+        int idx=-1;
+        int minlen=INT_MAX;
         while(r<n){
-            if(hash[s[r]]>0) cnt++;
-            hash[s[r]]--;
-            while(cnt==m){
-                if((r-l+1)<minlen){
-                    minlen=r-l+1;
-                    sind=l;
+            if(mpp[s[r]]>0) count++;
+            mpp[s[r]]--;
+            while(count==m){
+                if((r-l+1) < minlen){
+                    minlen= r-l+1;
+                    idx=l;
                 }
-                hash[s[l]]++;
-                if(hash[s[l]]>0){
-                    cnt--;
-                }
+                mpp[s[l]]++;
+                if(mpp[s[l]]>0) count --;
                 l++;
             }
             r++;
-
-
         }
-        return sind==-1 ? "" : s.substr(sind,minlen);
+        return idx==-1? "" :s.substr(idx,minlen);
     }
 };
