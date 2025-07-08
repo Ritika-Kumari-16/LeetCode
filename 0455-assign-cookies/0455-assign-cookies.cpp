9e33573e8e2631eec6b11644099e773217073259
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-08 12:40:25 +0530

    Time: 7 ms (73.24%), Space: 44.9 MB (10.16%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,22 +3,15 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
-        int n=g.size();
-        int m=s.size();
-        int count=0;
-        int i=0;
-        int j=0;
-        while(n>i && m>j){
+        int i=0,j=0;
+        int n= g.size();
+        int m =s.size();
+        while(i<n && j<m){
             if(g[i]<=s[j]){
-                count++;
-                i++,j++;
-                }
-            else if(g[i]>s[j]){
-                j++;
+                i++;
             }
-
+            j++;
         }
-        return count;
-        
+        return i;
     }
 };
