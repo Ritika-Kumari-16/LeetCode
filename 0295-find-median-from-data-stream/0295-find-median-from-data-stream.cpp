08a671ca69a37322c6f8d597123322242e21e78a
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-09 13:52:40 +0530

    Time: 69 ms (38.1%), Space: 125 MB (43.43%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,35 +1,35 @@
 class MedianFinder {
+    priority_queue<int>maxheap;
+    priority_queue<int,vector<int>,greater<int>>minheap;
 public:
-priority_queue<int>maxheap;
-priority_queue<int , vector<int>, greater<int>>minheap;
     MedianFinder() {
-        
     }
     
     void addNum(int num) {
-        if(maxheap.empty() || maxheap.top()> num){
+        if(maxheap.empty() || num<maxheap.top()){
             maxheap.push(num);
         }
-        else{
+        else
             minheap.push(num);
-        }
-        if(maxheap.size()< minheap.size()){
-            maxheap.push(minheap.top());
-            minheap.pop();
-        }
-        else if(maxheap.size()>minheap.size()+1){
+        if(maxheap.size() > minheap.size()+1){
             minheap.push(maxheap.top());
             maxheap.pop();
         }
-        
+        else if(minheap.size()>maxheap.size()){
+            maxheap.push(minheap.top());
+            minheap.pop();
+        }
     }
     
     double findMedian() {
-        if(maxheap.size()==minheap.size()){
-            double median= (maxheap.top() + minheap.top())/2.0;
-            return median;
+        double median=0;
+        if(minheap.size()==maxheap.size()){
+            median= (double)(minheap.top()+maxheap.top())/2.0;
         }
-        return maxheap.top();
+        else{
+            median=(double) maxheap.top();
+        }
+        return median;
     }
 };
 
