50f564e78f51f731fff06c27f122ee33ba08f112
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-10 17:36:43 +0530

    Time: 107 ms (29.53%), Space: 173.3 MB (61.84%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,46 +1,158 @@
+
 class LRUCache {
-public:
-    list<int>dll;
-    map<int,pair<list<int>::iterator,int>>mpp;
-    int n ;
-    LRUCache(int capacity) {
-        n=capacity;
+    public:
+    struct Node{
+    int key;
+    int value;
+    Node* prev ;
+    Node* next;
+    Node(int key1 , int val1){
+        key=key1;
+        value=val1;
     }
-    void makerecused(int key){
-        dll.erase(mpp[key].first);
-        dll.push_front(key);
-        mpp[key].first=dll.begin();
+};
+    map<int,Node*>mpp;
+    int capacity;
+    Node* head = new Node(-1,-1);
+    Node* tail = new Node(-1,-1);
+public:
+    LRUCache(int capacity1) {
+        capacity=capacity1;
+        head->next=tail;
+        tail->prev=head;
+    }
+    void insertnode(Node * temp){
+        Node * after= head->next;
+        head->next=temp;
+        temp->prev=head;
+        temp->next=after;
+        after->prev=temp;
+    }
+    void deletenode(Node* temp){
+        Node* before = temp->prev;
+        Node* after= temp->next;
+        before->next=after;
+        after->prev=before;
     }
     int get(int key) {
-        if(mpp.find(key)!=mpp.end()){
-            makerecused(key);
-            int val=mpp[key].second;
-            return val;
-        }
-        else{
+        if(mpp.find(key)==mpp.end()){
             return -1;
         }
+            Node* temp = mpp[key];
+            int val= temp->value;
+            deletenode(temp);
+            insertnode(temp);
+            mpp[key]=head->next;
+            return val;
         
     }
     
     void put(int key, int value) {
         if(mpp.find(key)!=mpp.end()){
-            mpp[key].second=value;
-            makerecused(key);
+            Node* temp = mpp[key];
+            temp->value= value;
+            deletenode(temp);
+            insertnode(temp);
+            mpp[key]=head->next;
         }
         else{
-            dll.push_front(key);
-            mpp[key]={dll.begin(),value};
-            n--;
+            Node* tempnode= new Node(key,value);
+            if(mpp.size()==capacity){
+                Node* temp1= tail->prev;
+                mpp.erase(temp1->key);
+                deletenode(tail->prev);
+            }
+            insertnode(tempnode);
+            mpp[key]=tempnode;
         }
-        if(n<0){
-            int key_=dll.back();
-            mpp.erase(key_);
-            dll.pop_back();
-            n++;
-        }
+        
     }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
+
+
+
+
+//  class LRUCache {
+//     public:
+//     struct Node{
+//         public:
+//             int key ;
+//             int val;
+//             Node* prev;
+//             Node* next;
+//             Node(int key_, int val_){
+//                 key=key_;
+//                 val=val_;
+//             }
+
+// };
+//     int cap;
+//     map<int,Node*>mpp;
+//     Node* head= new Node(-1,-1);
+//     Node* tail= new Node(-1,-1);
+
+// public:
+
+//     LRUCache(int capacity) {
+//         cap=capacity;
+//         mpp.clear();
+//         head->next=tail;
+//         tail->prev=head;
+//     }
+
+// void insertnode(Node* node){
+//     Node* temp = head->next;
+//     node->next=temp;
+//     temp->prev=node;
+//     head->next=node;
+//     node->prev=head;
+
+// }
+// void deletenode( Node* node){
+//     Node* temp = node->prev;
+//     Node* tempnxt=node->next;
+//     temp->next=tempnxt;
+//     tempnxt->prev=temp;
+// }
+//     int get(int key) {
+//         if(mpp.find(key)==mpp.end()) return -1;
+//         Node* reqval= mpp[key];
+//         mpp.erase(key);
+//         deletenode(reqval);
+//         insertnode(reqval);
+//         mpp[key]=head->next;
+//         return reqval->val; 
+//     }
+    
+//     void put(int key, int value) {
+//         if(mpp.find(key)!=mpp.end()){
+//             Node* reqval= mpp[key];
+//             reqval->val=value;
+//             mpp.erase(key);
+//             deletenode(reqval);
+//             insertnode(reqval);
+//             mpp[key]=head->next;
+//         }
+//         else{
+//             Node* newnode= new Node(key,value);
+//             if(mpp.size()==cap){
+//                 mpp.erase(tail->prev->key);
+//                 deletenode(tail->prev);
+
+//             }
+//             insertnode(newnode);
+//             mpp[key]=newnode;
+//         }
+        
+//     }
+// };
 
 /**
  * Your LRUCache object will be instantiated and called as such:
