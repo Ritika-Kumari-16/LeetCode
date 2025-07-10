139b3e959e4a57918dc3725839f70472864487c3
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-10 15:38:43 +0530

    Time: 5 ms (71.95%), Space: 19 MB (54.89%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,43 +1,48 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        stack<int>st;
+        int n = heights.size();
         int maxarea=0;
-        int n= heights.size();
+        stack<int>st;
         for(int i=0;i<n;i++){
-            while(!st.empty() && heights[st.top()]>heights[i]){
-                int ele= st.top();
+            while(!st.empty() && heights[st.top()]> heights[i]){
+                int nsidx=i;
+                int el= heights[st.top()];
                 st.pop();
-                int nse=i;
-                int pse= st.empty() ? -1: st.top();
-                maxarea=max(maxarea,heights[ele] * (nse-pse-1));
+                int psidx= st.empty()?-1:st.top();
+                maxarea=max(maxarea,(nsidx-psidx-1)*el);
             }
             st.push(i);
         }
         while(!st.empty()){
-            int nse= n;
-            int ele= st.top();
-            st.pop();
-            int pse= st.empty() ? -1:st.top();
-            maxarea=max(maxarea,heights[ele] * (nse-pse-1));
-        }
-        return maxarea;
+                int nsidx=n;
+                int el= heights[st.top()];
+                st.pop();
+                int psidx= st.empty()?-1:st.top();
+                maxarea=max(maxarea,(nsidx-psidx-1)*el);
+            }
+            return maxarea;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int n= matrix.size();
-        int m= matrix[0].size();
-        vector<vector<int>>presum(n,vector<int>(m,0));
-        int maxarea=0;
-        for(int j=0;j<m;j++){
-            int sum=0;
-            for(int i=0;i<n;i++){
-                sum+=(matrix[i][j]-'0');
-                if(matrix[i][j]=='0') {sum=0;}
-                presum[i][j]=sum;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>>prefix(n,vector<int>(m,0));
+        for(int i=0;i<m;i++){
+            prefix[0][i]=matrix[0][i]-'0';
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]=='0'){
+                    prefix[i][j]=0;
+                }
+                else{
+                    prefix[i][j]=prefix[i-1][j]+1;
+                }
             }
         }
+        int maxarea=0;
         for(int i=0;i<n;i++){
-            maxarea=max(maxarea,largestRectangleArea(presum[i]));
+            maxarea=max(maxarea,largestRectangleArea(prefix[i]));
         }
         return maxarea;
     }
