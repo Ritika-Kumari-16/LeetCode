33efa59aecbdedf3019bacc6fdf0e132dd099bc5
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-12 22:44:40 +0530

    Time: 229 ms (72.85%), Space: 10.8 MB (61.73%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -6,12 +6,14 @@ public:
         int m= board[0].size();
         if(row<0 || row>=n || col<0 || col>=m || visited[row][col]==1 ||board[row][col]!=word[i] ) return false;
         visited[row][col]=1;
-        bool found (solve(row-1,col,i+1,board,word,visited) 
+        if (solve(row-1,col,i+1,board,word,visited) 
                 || solve(row,col-1,i+1,board,word,visited) 
                 || solve(row,col+1,i+1,board,word,visited) 
-                || solve(row+1,col,i+1,board,word,visited) );
+                || solve(row+1,col,i+1,board,word,visited) ){
+                    return true;
+                }
         visited[row][col]=-1;
-        return found;
+        return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size();
