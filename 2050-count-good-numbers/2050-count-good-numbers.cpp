4fc95662ddcb3a4b233378b4f82f06fbdf437a1d
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-12 17:01:54 +0530

    Time: 0 ms (100%), Space: 7.9 MB (34.71%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.cpp
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -1,31 +1,26 @@
-/*
-
-    Time Complexity : O(logN).
-
-    Space Complexity : O(logN), Recursion stack space.
-
-*/
-
-#define mod 1000000007
 class Solution {
-private:
-    long long power(long long x, long long n){
-        if(n == 0){
-            return 1;
-        }
-        long long ans = power(x, n/2);
-        ans *= ans;
-        ans %= mod;
-        if(n%2==1){
-            ans *= x;
-            ans %= mod;
-        }
-        return ans;
-    }
 public:
+int mod = 1e9+7;
+    long long  pow_(long long x , long long n){
+        long long result=1;
+        while(n>0){
+            if(n%2==0){
+                x=(x*x)%mod;
+                n=n/2;
+            }
+            else{
+                result=(result*x)%mod;
+                n=n-1;
+            }
+        }
+        return result;
+    }
     int countGoodNumbers(long long n) {
-        long long numberOfOddPlaces = n/2;
-        long long numberOfEvenPlaces = n/2 + n%2;
-        return (power(5, numberOfEvenPlaces) * power(4, numberOfOddPlaces))%mod;
+        long long even= n/2+n%2;
+        long long odd= n/2;
+         long long res = (pow_(5, even) * pow_(4, odd)) % mod;
+        return (int) res;
+        
+        
     }
 };
