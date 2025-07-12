1ef498638f9ba8a10c926d21c1cf32fc967c69b6
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-12 17:58:30 +0530

    Time: 3 ms (32.62%), Space: 15.1 MB (23.89%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,23 +1,23 @@
 class Solution {
-  private:
-    void findsubsets(int ind,vector<vector<int>>&ans,vector<int>&sbst,vector<int>&arr,int n){
-        if(ind==n) {
-            ans.push_back(sbst);
-            return;
-        }
-        //taking ele
-        sbst.push_back(arr[ind]);
-        findsubsets(ind+1,ans,sbst,arr,n);
-        //not taking ele
-        sbst.pop_back();
-        findsubsets(ind+1,ans,sbst,arr,n);
-    }
 public:
+void generateSubsets(int i , int n , vector<int>&temp , vector<vector<int>>&ans,vector<int>nums){
+    if(i==n){
+        ans.push_back(temp);
+        return;
+    }
+    //take it 
+    temp.push_back(nums[i]);
+    generateSubsets(i+1,n,temp,ans,nums);
+    temp.pop_back(); 
+
+    //not take it
+    generateSubsets(i+1,n,temp,ans,nums);
+}
     vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
         vector<vector<int>>ans;
-        vector<int>subsets;
-        int n=nums.size();
-        findsubsets(0,ans,subsets,nums,n);
+        vector<int>temp;
+        generateSubsets(0,n,temp,ans,nums);
         return ans;
         
     }
