d9aa77ee0d024d66c804ff8c59eb45fc6138e012
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-12 19:48:38 +0530

    Time: 0 ms (100%), Space: 14 MB (82.83%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,26 +1,24 @@
 class Solution {
-private:
-    void findcomb( int ind,int target,vector<int>&arr,vector<vector<int>>
-    &ans,vector<int>&comb){
-            if(target==0){
-                ans.push_back(comb);
-                return ;
-        }
-        for(int i=ind;i<arr.size();i++){
-            if(i>ind && arr[i]==arr[i-1]) continue;
-            if(arr[i]>target) break;
-            comb.push_back(arr[i]);
-            findcomb(i+1,target-arr[i],arr,ans,comb);
-            comb.pop_back();
-        }
-    }
 public:
+void solve (int i, int n , int target , vector<vector<int>>&ans , vector<int>&nums , vector<int>&temp){
+    if(target==0) {
+        ans.push_back(temp);
+        return;}
+        
+    for(int idx=i;idx<n;++idx){
+        if(idx>i && nums[idx]==nums[idx-1]) continue;
+        if(nums[idx]>target) break;
+        temp.push_back(nums[idx]);
+        solve(idx+1,n,target-nums[idx],ans,nums,temp);
+        temp.pop_back();
+    }
+}
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(),candidates.end());
+        int n = candidates.size();
         vector<vector<int>>ans;
-        vector<int>comb;
-        findcomb(0,target,candidates,ans,comb);
+        vector<int>temp;
+        solve(0,n,target,ans,candidates,temp);
         return ans;
-        
     }
 };
