d5751493c23c64271949cb87ce9c817eccfde915
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-13 12:33:55 +0530

    Time: 3 ms (31.93%), Space: 16.5 MB (14.34%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,61 +1,18 @@
 class Solution {
 public:
-    int solve(int n, int m ,vector<vector<int>>&grid , vector<vector<int>>&dp){   
-        if(n==0&&m==0) return grid[0][0];
-        if(n<0||m<0) return INT_MAX;
-        if(dp[n][m]!=-1) return dp[n][m];
-        //left 
-        int left=solve(n,m-1,grid,dp);
-        int up=solve( n-1,m,grid,dp);
-        return dp[n][m]=grid[n][m]+min(left,up);
-
-    }
+int solve( int i , int j , int n , int m , vector<vector<int>>&grid , vector<vector<int>>&dp){
+    if(i==n-1 && j==m-1) return grid[n-1][m-1];
+    if(i>=n || j>=m) return INT_MAX;
+    if(dp[i][j]!=-1) return dp[i][j];
+    int  right=  solve(i,j+1,n,m,grid,dp);
+    int down =solve(i+1,j,n,m,grid,dp);
+    return dp[i][j] = grid[i][j] + min(right,down);
+}
     int minPathSum(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
+        int n = grid.size();
+        int m= grid[0].size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,grid,dp);
+        return solve(0,0,n,m,grid,dp);
         
     }
 };
-
-// class Solution {
-// public:
-// int solve(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& dp){
-//     if(i==0&&j==0)return grid[0][0];
-//     if(i<0||j<0)return INT_MAX;
-//     if(dp[i][j]!=-1)return dp[i][j];
-//     int up=solve(i-1,j, grid,dp);
-//     int left=solve(i,j-1, grid,dp);
-//     return dp[i][j]=grid[i][j]+min(up,left);
-//     return grid[i][j]+min(up,left);
-
-// }
-//     int minPathSum(vector<vector<int>>& grid) {
-//         //recursion
-//         int n=grid.size();
-//         int m=grid[0].size();
-//         vector<vector<int>>dp(n,vector<int>(m,-1));
-//         return solve(n-1,m-1,grid,dp);
-
-//         //Tabulation
-//         // for(int i=0;i<n;i++){
-//         //     vector<int>temp(m,0);
-//         //     for(int j=0;j<m;j++){
-            
-//         //         if(i==0&&j==0)temp[j]=grid[i][j];
-//         //         else{
-//         //         int up=grid[i][j];
-//         //         if(i>0)up+=dp[j];
-//         //         else up+=1e9;
-//         //         int left=grid[i][j];
-//         //         if(j>0)left+=temp[j-1];
-//         //         else left+=1e9;
-//         //         temp[j]=min(up,left);
-//         //         }
-//         //     }
-//         //     dp=temp;
-//         // }
-//         // return dp[m-1];
-//     }
-// };
