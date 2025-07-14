f6ac883db0b9a7689dced0d699e01ef625193b67
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-14 22:01:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
-    int numDistinct(string s, string t) {
-        int n= s.size();
-        int m= t.size();
-        vector<double>dp(m+1,0);
-        dp[0]=1;
-        for(int i=1;i<=n;i++){
-            for(int j=m;j>=1;j--){
-                if(s[i-1]==t[j-1]){
-                    dp[j]=dp[j-1]+dp[j];
+    int numDistinct(string text1, string text2) {
+        int n= text1.size();
+        int m = text2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++) dp[i][m]=1;
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(text1[i]==text2[j]){
+                    dp[i][j]=dp[i+1][j]+dp[i+1][j+1];
                 }
-
+                else{
+                    dp[i][j]=dp[i+1][j];
+                }
             }
         }
-        return (int)dp[m];
+    return (int)dp[0][0];
     }
 };
-
-
-
-// class Solution {
-// public:
-//    memoization 
-//     int solve( int i , int j , string &s, string &t, vector<vector<int>>&dp){
-//         if(j<0) return 1;
-//         if(i<0) return 0;
-//         if(dp[i][j]!=-1) return dp[i][j];
-//         if(s[i]==t[j]){
-//             return dp[i][j]=solve(i-1,j-1,s,t,dp)+solve(i-1,j,s,t,dp);
-//         }
-//         return dp[i][j]=solve(i-1,j,s,t,dp);
-//     }
-//     int numDistinct(string s, string t) {
-//         int n= s.size();
-//         int m = t.size();
-//         vector<vector<int>>dp(n,vector<int>(m,-1));
-//         return solve(n-1,m-1,s,t,dp);
-        
-//     }
-// };
