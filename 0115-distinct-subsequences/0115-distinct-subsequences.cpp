f6b008a06e01a5fa69a64fb76659d7c8f331eee2
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-14 22:02:26 +0530

    Time: 30 ms (59.12%), Space: 44.4 MB (22.33%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -3,7 +3,7 @@ public:
     int numDistinct(string text1, string text2) {
         int n= text1.size();
         int m = text2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        vector<vector<double>>dp(n+1,vector<double>(m+1,0));
         for(int i=0;i<=n;i++) dp[i][m]=1;
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
