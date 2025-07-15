c48b99661bcaf70e46e74f312ccd4ee98b9beec3
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-15 19:49:05 +0530

    Time: 3 ms (35.28%), Space: 16 MB (70.93%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -2,37 +2,40 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>>dp(n+2,vector<int>(2, 0));
-        // Traverse the prices array in reverse order
+        vector<vector<int>> dp(n + 2, vector<int>(2, 0));
         for (int i = n - 1; i >= 0; i--) {
-
-                    dp[i][1]= max(-prices[i] + dp[i+1][0], dp[i+1][1]);
-                    dp[i][0] = max(prices[i] + dp[i+2][1], dp[i+1][0]);
-                }// Update the current state
-
-        return dp[0][1]; // Maximum profit with buying allowed on the first day
+            for(int buy=1;buy>=0;buy--){
+                if(buy%2==0){
+                    dp[i][buy] =max(-prices[i] + dp[i + 1][1], dp[i +1][buy]); // buy skip
+                }
+                else{
+                    dp[i][buy] =max(prices[i] + dp[i + 2][0], dp[i + 1][buy]); // sell skip
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
 
-// // memoization and recursion
 // class Solution {
 // public:
-//     int solve(int ind, int buy, int n , vector<int>&prices , vector<vector<int>>&dp){
-//         if(ind>=n) return 0;
-//         if(dp[ind][buy]!=-1){ return dp[ind][buy];}
-//         int profit=0;
-//         if(buy){
-//             profit=max(-prices[ind]+solve(ind+1,0,n,prices,dp),solve(ind+1,1,n,prices,dp));
-//         }
-//         else{
-//             profit=max(prices[ind]+solve(ind+2,1,n,prices,dp),solve(ind+1,0,n,prices,dp));
-//         }
-//         return dp[ind][buy]=profit;
+// int solve(int i , int buy ,int n , vector<int>&prices,vector<vector<int>>&dp){
+//     if(i>=n) return 0;
+//     if(buy>=n) return 0;
+//     if(dp[i][buy]!=-1) return dp[i][buy];
+//     //buy
+//     if(buy%2==0){
+//         return dp[i][buy]=max(-prices[i] + solve(i+1,buy+1,n,prices,dp) , solve(i+1,buy,n,prices,dp));
 //     }
+//     //sell
+//     else{
+//         return dp[i][buy]=max(prices[i]+solve(i+2,buy+1,n,prices,dp) , solve(i+1,buy,n,prices,dp));
+//     }
+// }
 //     int maxProfit(vector<int>& prices) {
-//         int n= prices.size();
-//         vector<vector<int>>dp(n+1,vector<int>(2,-1));
-//         return solve(0,1,n,prices,dp);
+//         int n = prices.size();
+//         vector<vector<int>>dp(n+1,vector<int>(n,-1));
+//         return solve(0,0,n,prices,dp);
         
 //     }
 // };
