281b2911972dae3a699b1ce93b82d3466b12b98c
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-15 10:01:15 +0530

    Time: 28 ms (75.14%), Space: 14.9 MB (67.24%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        vector<vector<bool>> dp(s.size() + 1, vector(p.size() + 1, false));
-        dp[0][0] = true;
-        for (int j = 0; j < p.size() && p[j] == '*'; ++j) {
-            dp[0][j + 1] = true;
-        }
-        
-        for (int i = 1; i <= s.size(); ++i) {
-            for (int j = 1; j <= p.size(); ++j) {
-                if (p[j - 1] == '*') {
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                } else {
-                    dp[i][j] = (s[i - 1] == p[j - 1] || p[j - 1] == '?') && dp[i - 1][j - 1];
-                }
+        int n = s.size();
+        int m = p.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+        dp[n][m]=true;
+        for(int i=m-1;i>=0;i--){
+            if(p[i]=='*'){
+                dp[n][i]=dp[n][i+1];
+            }
+            else{
+                break;
             }
         }
 
-        return dp[s.size()][p.size()];
+        for(int i= n-1 ;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(s[i]==p[j] || p[j]=='?'){
+                    dp[i][j] = dp[i+1][j+1];
+                }
+                else if(p[j] == '*'){
+                    dp[i][j]=(dp[i][j+1] || dp[i+1][j+1] || dp[i+1][j]);
+                }
+                else{
+                    dp[i][j]=false;
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
