8d6a7bc6ee53f6638454cba0b277e77df871d522
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-17 17:45:05 +0530

    Time: 0 ms (100%), Space: 17.2 MB (33.85%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m =grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        queue<pair<pair<int,int>,int>>q;
+        int n = grid.size();
+        int m= grid[0].size();
+        vector<vector<int>>visited(n,vector<int>(m,0));
+        queue<pair<int,int>>q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    q.push({{i,j},0});
+                    q.push({i,j});
+                    visited[i][j]=1;
                 }
             }
         }
         int time=0;
-        int delrow[]={-1,0,0,+1};
-        int delcol[]={0,-1,+1,0};
+        int drow[]={-1,0,0,1};
+        int dcol[]={0,-1,1,0};
         while(!q.empty()){
-            int r=q.front().first.first;
-            int c=q.front().first.second;
-            int t=q.front().second;
-            q.pop();
-            time=max(time,t);
-            for(int i=0;i<4;i++){
-                int nr=r+delrow[i];
-                int nc=c+delcol[i];
-                if(nr<n&&nr>=0&&nc<m&&nc>=0&& grid[nr][nc]==1 && vis[nr][nc]==0){
-                    vis[nr][nc]=2;
-                    q.push({{nr,nc},t+1});
+            int size=q.size();
+            bool changed=false;
+            for(int i=0;i<size;i++){
+                int row= q.front().first;
+                int col=q.front().second;
+                q.pop();
+                for(int j=0;j<4;j++){
+                    int nrow=row+drow[j];
+                    int ncol=col+dcol[j];
+                    if(nrow<0 || nrow >=n || ncol<0 || ncol>=m || grid[nrow][ncol]==0 || grid[nrow][ncol]==2 || visited[nrow][ncol]) continue;
+                    visited[nrow][ncol]=1;
+                    q.push({nrow,ncol});
+                    changed=true;
                 }
+                
             }
-
+            if(changed) time++;
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1 && vis[i][j]==0){
-                    return -1;
-                    }
-                }
+                if(grid[i][j] == 1 && !visited[i][j]) return -1;
             }
+        }
         return time;
-        
     }
 };
