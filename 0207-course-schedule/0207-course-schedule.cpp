3ecfe0aa892bc341d2b0902fc176d098d71ef7d0
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-18 22:31:08 +0530

    Time: 13 ms (12.51%), Space: 19.2 MB (74.7%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,48 +1,34 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int counter = 0;
-        if (numCourses <= 0) {
-            return true;
+        vector<vector<int>>adj(numCourses);
+        for(auto &it :prerequisites ){
+            adj[it[0]].push_back(it[1]);
         }
-
-        // Initialize inDegree and graph
-        vector<int> inDegree(numCourses, 0);
-        vector<vector<int>> graph(numCourses);
-
-        // Build the graph and update inDegree for each node
-        for (const auto& edge : prerequisites) {
-            int parent = edge[1];
-            int child = edge[0];
-            graph[parent].push_back(child);
-            inDegree[child]++;
-        }
-
-        // Initialize the queue with courses having no prerequisites (inDegree =
-        // 0)
-        queue<int> sources;
-        for (int i = 0; i < numCourses; i++) {
-            if (inDegree[i] == 0) {
-                sources.push(i);
+        vector<int>indegree(numCourses,0);
+        for(int i=0;i<numCourses;i++){
+            for(auto &it :adj[i]){
+                indegree[it]++;
             }
         }
-
-        // Process nodes with no prerequisites
-        while (!sources.empty()) {
-            int course = sources.front(); // dequeue
-            sources.pop();
-            counter++;
-
-            // Process all the children of the current course
-            for (int child : graph[course]) {
-                inDegree[child]--;
-                if (inDegree[child] == 0) {
-                    sources.push(child); // enqueue child if inDegree becomes 0
+        queue<int>q;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        int courses=0;
+        while(!q.empty()){
+            int node= q.front();
+            q.pop();
+            courses++;
+            for(auto &it :adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
                 }
             }
         }
-
-        // If we processed all courses, return true
-        return counter == numCourses;
+        return courses==numCourses;
     }
 };
