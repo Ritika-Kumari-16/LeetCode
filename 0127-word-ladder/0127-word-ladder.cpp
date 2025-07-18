03de1a74d0c0698c4f0ceb4884fb3699813e63fd
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-18 14:15:10 +0530

    Time: 44 ms (93.29%), Space: 21.2 MB (65.09%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> dict(wordList.begin(), wordList.end());
-        queue<string> todo;
-        todo.push(beginWord);
-        int ladder = 1;
-        while (!todo.empty()) {
-            int n = todo.size();
-            for (int i = 0; i < n; i++) {
-                string word = todo.front();
-                todo.pop();
-                if (word == endWord) {
-                    return ladder;
+        unordered_set<string>st;
+        for(auto &word: wordList){
+            st.insert(word);
+        }
+        if(st.find(endWord)==st.end()) return 0;
+        queue<pair<string,int>>q;
+        q.push({beginWord,1});
+        while(!q.empty()){
+            string word= q.front().first;
+            int dist=q.front().second;
+            q.pop();
+            if(word==endWord) return dist;
+            for(int i=0;i<word.size();i++){
+                char original =word[i];
+                for(char ch='a';ch<='z';ch++){
+                    word[i]=ch;
+                    if(st.find(word)!=st.end()){
+                        q.push({word,dist+1});
+                        st.erase(word);
+                    }
                 }
-                dict.erase(word);
-                for (int j = 0; j < word.size(); j++) {
-                    char c = word[j];
-                    for (int k = 0; k < 26; k++) {
-                        word[j] = 'a' + k;
-                        if (dict.find(word) != dict.end()) {
-                            todo.push(word);
-                        }
-                     }
-                    word[j] = c;
-                }
+                word[i]=original;
             }
-            ladder++;
         }
         return 0;
     }
