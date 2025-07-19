f83e70eff8763c49fb547b2637496c4ee08015b5
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-19 10:30:13 +0530

    Time: 50 ms (45.89%), Space: 66.2 MB (37.56%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,45 +1,36 @@
 class Solution {
 public:
-	bool dfsCheck(int node, vector<vector<int>> &adj, vector<int>&vis,vector<int>&pathVis, vector<int>&check) {
-		vis[node] = 1;
-		pathVis[node] = 1;
-		check[node] = 0;
-		// traverse for adjacent nodes
-		for (auto it : adj[node]) {
-			// when the node is not visited
-			if (!vis[it]) {
-			if (dfsCheck(it, adj, vis, pathVis, check) == true) {
-					check[node] = 0;
-					return true;
-				}
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int N= graph.size();
+        vector<vector<int>>adj(N);
+        vector<int>indegree(N,0);
+        for(int i=0;i<N;i++){
+            for(auto &it: graph[i]){
+                adj[it].push_back(i);
+                indegree[i]++;
+            }
+        }
 
-			}
-			// if the node has been previously visited
-			// but it has to be visited on the same path
-			else if (pathVis[it]) {
-				check[node] = 0;
-				return true;
-			}
-		}
-		check[node] = 1;
-		pathVis[node] = 0;
-		return false;
-	}
-
-    vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
-        int V= adj.size();
-		vector<int>vis(V,0);
-		vector<int>pathVis(V,0);
-		vector<int>check(V,0);
-		vector<int> safeNodes;
-		for (int i = 0; i < V; i++) {
-			if (!vis[i]) {
-				dfsCheck(i, adj, vis, pathVis, check);
-			}
-		}
-		for (int i = 0; i < V; i++) {
-			if (check[i] == 1) safeNodes.push_back(i);
-		}
-		return safeNodes;
+        queue<int>q;
+        vector<int>ans;
+        for(int i=0;i<N;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto &it :adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+        
     }
 };
