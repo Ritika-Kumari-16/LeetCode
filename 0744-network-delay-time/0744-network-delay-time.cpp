4ed9431744eb0926da9c4465bd1cce37fabb2b1f
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-19 16:50:05 +0530

    Time: 83 ms (64.23%), Space: 47.9 MB (28.98%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,47 +1,31 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int N, int src) {
-        vector<pair<int,int>>adj[N+1];
-        for(auto it:times)
-        {
-            int u = it[0];
-            int v = it[1];
-            int wt = it[2];
-            adj[u].push_back({v,wt});
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int,int>>>adj(n+1);
+        for(auto it: times){
+            adj[it[0]].push_back({it[1],it[2]});
         }
-        
-        vector<int>dist(N+1, 1e9);
-        
-        queue<int>q;
-        q.push(src);
-        dist[src]=0;
-        dist[0]=0;
-        
-        while(!q.empty())
-        {
-            int front = q.front();
-            q.pop();
-            
-            int distance = dist[front];
-            
-            for(auto neigh: adj[front])
-            {
-                int newd = distance + neigh.second;
-                if(newd<dist[neigh.first])
-                {
-                    q.push(neigh.first);
-                    dist[neigh.first]=newd;
+        vector<int>mintime(n+1,INT_MAX);
+        mintime[k]=0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
+        pq.push({0,k});
+        while(!pq.empty()){
+            int time=pq.top().first;
+            int node=pq.top().second;
+            pq.pop();
+            for(auto it: adj[node]){
+                if(mintime[it.first]>it.second+time){
+                    mintime[it.first]=it.second+time;
+                    pq.push({mintime[it.first],it.first});
                 }
             }
         }
-    
-        
-        int ans = -1;
-        for(int i=1;i<=N;i++)
-        {
-            if(dist[i]==1e9) return -1;
-            ans = max(ans, dist[i]);
+        int mini=INT_MIN;
+        for(int i=1;i<n+1;i++){
+            if(mintime[i]==INT_MAX) return -1;
+            mini=max(mini,mintime[i]);
         }
-        return ans;
+        return mini;
     }
+    
 };
