701f964a0bafee3fd9d66bea6efb23a61f8ade85
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-21 22:08:12 +0530

    Time: 13 ms (77.03%), Space: 24.3 MB (43.71%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
@@ -1,64 +1,88 @@
-class DisjointSet{
-    
+class DisjointSet {
+    vector<int>parent;
+    vector<int>rank;
+    vector<int>size;
 public:
-    vector<int>par,rank;
-    DisjointSet ( int n){
-        rank.resize(n,0);
-        par.resize(n);
-        for(int i=0;i<n;i++){
-            par[i]=i;
-        }
-    }
-    int findpar(int node){
-        if(par[node]==node){
-            return node;
-        }
-        return par[node]=findpar(par[node]);
-    }
-    void unionSet( int u , int v){
-        int upu=findpar(u);
-        int upv=findpar(v);
-        if(upu==upv) return ;
-        if(rank[upu]<rank[upv]){
-            par[upu]=upv;
-        }
-        else if(rank[upu]>rank[upv]){
-            par[upv]=upu;
-        }
-        else{
-            par[upu]=upv;
-            rank[upv]++;
-        }
+    DisjointSet(int n) {
+     parent.resize(n+1,0);
+     rank.resize(n+1,0);
+     size.resize(n+1,1);
+     for(int i=0;i<=n;i++){
+        parent[i]=i;
+     }
     }
 
+    bool find(int u, int v) {
+        return findparent(u)==findparent(v);
+    }
+    int findparent(int u){
+        if(parent[u]==u) return u;
+        return parent[u]=findparent(parent[u]);
+    }
+
+    void unionByRank(int u, int v) {
+        int upu=findparent(u);
+        int upv=findparent(v);
+        if(upu==upv) return ;
+        if(rank[upu]<rank[upv]){
+            parent[upu]=upv;
+        }
+        else if(rank[upu]>rank[upv]){
+            parent[upv]=upu;
+        }
+        else{
+            parent[upv]=upu;
+            rank[upu]++;
+        }
+     
+    }
+
+    void unionBySize(int u, int v) {
+        int upu=findparent(u);
+        int upv=findparent(v);
+        if(upu==upv) return ;
+        if(size[upu]<size[upv]){
+            parent[upu]=upv;
+            size[upv]+=size[upu];
+        }
+        else if(size[upu]>size[upv]){
+            parent[upv]=upu;
+            size[upu]+=size[upv];
+        }
+        else {
+            parent[upv]=upu;
+            size[upu]+=size[upv];
+        }
+        
+       
+    }
 };
+
 class Solution {
 public:
     int removeStones(vector<vector<int>>& stones) {
         int n= stones.size();
         int maxrow=0;
         int maxcol=0;
-        for( auto& it: stones){
+        for(auto &it: stones){
             maxrow=max(maxrow,it[0]);
             maxcol=max(maxcol,it[1]);
-
         }
-        DisjointSet ds(maxrow+maxcol+2);
-        unordered_set<int>visitednodes;
-        for(auto it:stones){
-            int nrow=it[0];
-            int ncol=it[1]+maxrow+1;
-            ds.unionSet(nrow,ncol);
-            visitednodes.insert(nrow);
-            visitednodes.insert(ncol);
+        DisjointSet ds(maxrow+maxcol+1);
+        unordered_map<int,int>mpp;
+        for(auto it: stones){
+            int row=it[0];
+            int col=it[1];
+            ds.unionBySize(row,maxrow+1+col);
+            mpp[row]=1;
+            mpp[maxrow+1+col]=1;
         }
-        int cnt=0;
-        for(auto it: visitednodes){
-            if(ds.findpar(it)==it){
-                cnt++;
+        int count=0;
+        for(auto it: mpp){
+            if(ds.findparent(it.first)==it.first){
+                count++;
             }
         }
-        return n-cnt;
-        
+        return n-count;
     }
 };
