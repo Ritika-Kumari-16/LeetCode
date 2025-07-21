d37b3759f7a91bef87d26c6b1499b36b992aabc1
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-21 23:59:24 +0530

    Time: 0 ms (100%), Space: 12.9 MB (75.4%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-    bool isvalid( int row, int col ,vector<vector<int>>& grid , int n ){
-        return row>=0 && row<n && col >=0 && col<n;
-    }
     int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size();
+        int n= grid.size();
         vector<vector<int>>visited(n,vector<int>(n,0));
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<>>pq;
         pq.push({grid[0][0],{0,0}});
         visited[0][0]=1;
-        int dr[]={-1,0,0,1}; 
+        int dr[]={-1,0,0,1};
         int dc[]={0,-1,1,0};
-        int leastTime=0;
         while(!pq.empty()){
-            int time= pq.top().first;
-            int row = pq.top().second.first;
-            int col = pq.top().second.second;
-            leastTime=max(leastTime,time);
+            int time=pq.top().first;
+            int row=pq.top().second.first;
+            int col=pq.top().second.second;
             pq.pop();
-            if(row==n-1 && col==n-1) return leastTime;
+            if(row==n-1 && col==n-1) return time;
             for(int i=0;i<4;i++){
-                int nrow= row+dr[i];
-                int ncol= col+dc[i];
-                if(isvalid(nrow,ncol,grid ,n ) && visited[nrow][ncol]==0){
-                    pq.push({grid[nrow][ncol],{nrow,ncol}});
+                int nrow=row+dr[i];
+                int ncol=col+dc[i];
+                if( nrow>=0 && nrow<n && ncol>=0 && ncol<n && !visited[nrow][ncol]){
                     visited[nrow][ncol]=1;
+                    int maxtime=max(time,grid[nrow][ncol]);
+                    pq.push({maxtime,{nrow,ncol}});
                 }
             }
-
         }
-
         return -1;
     }
 };
