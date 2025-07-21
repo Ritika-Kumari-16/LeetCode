8f46a8241993cc9b3e85dacb4080f7579d8b4f37
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-21 14:55:58 +0530

    Time: 1 ms (91.37%), Space: 42.6 MB (75.79%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -1,61 +1,88 @@
-class DisjointSet{
-    
+class DisjointSet {
 public:
-    vector<int>par,rank;
-    DisjointSet ( int n){
-        rank.resize(n,0);
-        par.resize(n);
-        for(int i=0;i<n;i++){
-            par[i]=i;
-        }
-    }
-    int findpar(int node){
-        if(par[node]==node){
-            return node;
-        }
-        return par[node]=findpar(par[node]);
-    }
-    void dounionSet( int u , int v){
-        int upu=findpar(u);
-        int upv=findpar(v);
-        if(upu==upv) return ;
-        if(rank[upu]<rank[upv]){
-            par[upu]=upv;
-        }
-        else if(rank[upu]>rank[upv]){
-            par[upv]=upu;
-        }
-        else{
-            par[upu]=upv;
-            rank[upv]++;
-        }
+    vector<int>parent;
+    vector<int>rank;
+    vector<int>size;
+public:
+    DisjointSet(int n) {
+     parent.resize(n+1,0);
+     rank.resize(n+1,0);
+     size.resize(n+1,1);
+     for(int i=0;i<=n;i++){
+        parent[i]=i;
+     }
     }
 
+    bool find(int u, int v) {
+        return findparent(u)==findparent(v);
+    }
+    int findparent(int u){
+        if(parent[u]==u) return u;
+        return parent[u]=findparent(parent[u]);
+    }
+
+    void unionByRank(int u, int v) {
+        int upu=findparent(u);
+        int upv=findparent(v);
+        if(upu==upv) return ;
+        if(rank[upu]<rank[upv]){
+            parent[upu]=upv;
+        }
+        else if(rank[upu]>rank[upv]){
+            parent[upv]=upu;
+        }
+        else{
+            parent[upv]=upu;
+            rank[upu]++;
+        }
+     
+    }
+
+    void unionBySize(int u, int v) {
+        int upu=findparent(u);
+        int upv=findparent(v);
+        if(upu==upv) return ;
+        if(size[upu]<size[upv]){
+            parent[upu]=upv;
+            size[upv]+=size[upu];
+        }
+        else if(size[upu]>size[upv]){
+            parent[upv]=upu;
+            size[upu]+=size[upv];
+        }
+        else {
+            parent[upv]=upu;
+            size[upu]+=size[upv];
+        }
+        
+       
+    }
 };
+
 class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
-        int extracables=0;
+        int conntotal=0;
+        int num=0;
+        int notconntotal=0;
         DisjointSet ds(n);
-        for (auto it: connections){
+        for(auto &it: connections){
             int u= it[0];
-            int v= it[1];
-            if(ds.findpar(u)==ds.findpar(v)){
-                extracables++;
+            int v=it[1];
+            if(ds.find(u,v)){
+                conntotal++;
             }
             else{
-                ds.dounionSet(u,v);
+                ds.unionBySize(u,v);
             }
         }
-        int cntforcom=0;
-        for( int i=0;i<n;i++){
-            if(ds.par[i]==i){
-                cntforcom++;
+        for(int i=0;i<n;i++){
+            if(ds.findparent(i)==i){
+                notconntotal++;
             }
         }
-        int ans= cntforcom-1;
-        if(extracables>=ans) return ans;
-        return -1;
+        if(notconntotal-1<=conntotal) return notconntotal-1;
+        return  -1;
         
     }
 };
