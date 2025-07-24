93123d5ba70e3f19c44286bd2042a5d2c7c7c80b
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-24 20:11:26 +0530

    Time: 9 ms (20.86%), Space: 15.5 MB (49.95%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,64 +9,57 @@
  */
 class Solution {
 public:
-    void markparents(TreeNode* root , unordered_map<TreeNode*, TreeNode*>&parent_tracker){
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            TreeNode* node= q.front();
-            q.pop();
-            if(node->left){
-                parent_tracker[node->left]=node;
-                q.push(node->left);
-            }
-            if(node->right){
-                parent_tracker[node->right]=node;
-                q.push(node->right);
-            }
-        }
+void findparent(TreeNode* root , unordered_map<TreeNode* , TreeNode*>&parentmpp){
+    queue<TreeNode*>q;
+    q.push(root);
+    parentmpp[root]=NULL;
+    while(!q.empty()){
+        TreeNode* node=q.front();
+        q.pop();
+        if(node->left) {
+            parentmpp[node->left]=node;
+            q.push(node->left);}
+
+        if(node->right) {
+            parentmpp[node->right]=node;
+            q.push(node->right);}
+
     }
+}
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        vector<int>ans;
-        if(target==NULL) return {};
-        unordered_map<TreeNode*, TreeNode*>parent_tracker;
-        markparents(root,parent_tracker);
-        unordered_map<TreeNode* , bool>visited;
+        if(root==NULL) return {};
+        unordered_map<TreeNode* , TreeNode*>parentmpp;
+        findparent(root,parentmpp);
         queue<TreeNode*>q;
-        q.push(target);
+        unordered_map<TreeNode*,bool>visited;
         visited[target]=true;
-        int dis=0;
+        q.push(target);
         while(!q.empty()){
-            int siz= q.size();
-            if(dis==k){
-                break;
-            }
-            dis++;
-            for(int i=0;i<siz;i++){
-                TreeNode* current= q.front();
+            if(k==0) break;
+            k--;
+            int s=q.size();
+            for(int i=0;i<s;i++){
+                TreeNode* node=q.front();
                 q.pop();
-                if(current->left && !visited[current->left]){
-                    q.push(current->left);
-                    visited[current->left]=true;
+                if(node->left && !visited[node->left]){
+                    q.push(node->left);
+                    visited[node->left]=true;
                 }
-                if(current->right && !visited[current->right]){
-                    q.push(current->right);
-                    visited[current->right]=true;
+                if(node->right && !visited[node->right]){
+                    q.push(node->right);
+                    visited[node->right]=true;
                 }
-                if(parent_tracker[current] && !visited[parent_tracker[current]]){
-                    q.push(parent_tracker[current]);
-                    visited[parent_tracker[current]]=true;
+                if(parentmpp[node]!=NULL && !visited[parentmpp[node]]){
+                    q.push(parentmpp[node]);
+                    visited[parentmpp[node]]=true;
                 }
-
             }
-
         }
-        
+        vector<int>ans;
         while(!q.empty()){
-            TreeNode* curr=q.front();
+            ans.push_back(q.front()->val);
             q.pop();
-            ans.push_back(curr->val);
         }
         return ans;
-        
     }
 };
