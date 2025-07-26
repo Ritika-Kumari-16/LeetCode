1d8732a11cfa644794ba45228ee002897a97668b
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-27 00:04:25 +0530

    Time: 0 ms (100%), Space: 15.6 MB (28.37%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,34 +11,18 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if (head == NULL || head->next == NULL) return head; // Edge case: empty or single-node list
-    
-    ListNode* odd = new ListNode(-1);  //Intialize odd LL with -1
-    ListNode* ptr = odd;           
-    
-    ListNode* even = new ListNode(-1); //Intialize even LL with -1
-    ListNode* ptr2 = even;        
-    
-    ListNode* temp = head;  //put temp on head
-    int count = 1;      //count=1;
-
-    while (temp) {     //Jab tak temp null nhi hota
-        if (count % 2 != 0) {
-            ptr->next = temp;  // Add to odd list
-            ptr = ptr->next;   // Move pointer
-        } else {
-            ptr2->next = temp; // Add to even list
-            ptr2 = ptr2->next; // Move pointer
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* odd=head;
+        ListNode* even=head->next;
+        ListNode* evenhead=head->next;
+        while(even && even->next){
+            odd->next=odd->next->next;
+            even->next=even->next->next;
+            odd=odd->next;
+            even=even->next;
         }
-        temp = temp->next;
-        count++;
-    }
-    //odd= -1->1->5  ptr
-    //even= -1->3->7  ptr2
-    
-    ptr->next = even->next;   // Connect odd list with even list
-    ptr2->next = NULL;        // Terminate the even list
-
-    return odd->next;         // Return the new head (skipping the dummy node)
+        odd->next=evenhead;
+        return head;
+        
     }
 };
