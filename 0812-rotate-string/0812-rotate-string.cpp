7e3eea44475c726fd1981d7183f9cbb2ab01bd49
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-27 18:31:11 +0530

    Time: 2 ms (12.65%), Space: 8.6 MB (16.57%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -4,7 +4,7 @@ public:
         if(goal.size()!=s.size() ) return false;
         string s1= s+s;
         int n= goal.size();
-        for(int i=0;i<s1.size()-n;i++){
+        for(int i=0;i<=s1.size()-n;i++){
             if(s1.substr(i,n)==goal) return true;
         }
         return false;
