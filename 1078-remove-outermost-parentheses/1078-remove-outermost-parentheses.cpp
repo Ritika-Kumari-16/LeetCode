a8d81afb73ace4f2197aabf6138a324632e0a78e
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-27 16:57:07 +0530

    Time: 0 ms (100%), Space: 9 MB (24.34%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,56 +1,29 @@
-// class Solution {
-// public:
-//     string removeOuterParentheses(string s) {
-//         stack<char>st;
-//         string ans="";
-//         int n= s.size();
-//         for(int i=0;i<n;i++){
-//             if(st.empty()){
-//                 st.push(s[i]);
-//             }
-//             else{
-//                 if(s[i]=='('){
-//                     st.push(s[i]);
-//                     ans+=s[i];
-//                 }
-//                 else{
-//                     st.pop();
-//                     if(!st.empty()){
-//                         ans+=s[i];
-//                     }
-//                 }
-//             }
-//         }
-//         return ans;
-        
-//     }
-// };
-
-#include <string>
-using namespace std;
-
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string ans = "";
-        int depth = 0;  // Track the depth of nested parentheses
-
-        for (char c : s) {
-            if (c == '(') {
-                // Only add to result if it's not an outermost '('
-                if (depth > 0) {
-                    ans += c;
+        string ans="";
+        int open=0;
+        for(auto &ch : s){
+            if(ch=='(') {
+                if(open==0){
+                    open++;
                 }
-                depth++;
-            } else {
-                depth--;
-                // Only add to result if it's not an outermost ')'
-                if (depth > 0) {
-                    ans += c;
+                else{
+                    ans+=ch;
+                    open++;
                 }
             }
-        }
+            else{
+                if(open==1){
+                    open--;
+                }
+                else{
+                    ans+=ch;
+                    open--;
+                }
+            }
 
+        }
         return ans;
     }
 };
