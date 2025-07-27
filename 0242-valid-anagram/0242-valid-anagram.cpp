5f0d71a08f45a17436cc2b4371d1430b3db373a8
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-07-27 18:23:48 +0530

    Time: 0 ms (100%), Space: 9.7 MB (74.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,15 +3,14 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
         vector<int>freq(26,0);
-        vector<int>freq1(26,0);
         for(int i=0;i<s.size();i++){
             freq[s[i]-'a']++;
         }
         for(int i=0;i<t.size();i++){
-            freq1[t[i]-'a']++;
+            freq[t[i]-'a']--;
         }
         for(int i=0;i<26;i++){
-            if(freq[i]!=freq1[i]) return false;
+            if(freq[i]!=0) return false;
         }
         return true;
     }
