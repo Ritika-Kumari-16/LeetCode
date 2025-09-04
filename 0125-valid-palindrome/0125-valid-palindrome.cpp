f12ae019e8b66a88f04c0270f915bf61a2a7672e
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-04 11:28:52 +0530

    Time: 0 ms (100%), Space: 10 MB (42.51%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-       int start=0;
-       int end=s.size()-1;
-       while(start<=end){
-           if(!isalnum(s[start])){start++; continue;}
-           if(!isalnum(s[end])){end--;continue;}
-           if(tolower(s[start])!=tolower(s[end]))return false;
-           else{
-               start++;
-               end--;
-           }
-       }
-       return true;
-}
+        int i=0;
+        int n=s.size()-1;
+        while(i<=n){
+            if(!isalnum(s[i])){
+                i++;
+                continue;
+            }
+            if(!isalnum(s[n])){
+                n--;
+                continue;
+            }
+            if(tolower(s[i])!=tolower(s[n])) return false;
+            i++;
+            n--;
+        }
+        return true;
+    }
 };
