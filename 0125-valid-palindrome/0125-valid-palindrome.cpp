c5ff3f69a86bfb52d9c4ba76aed36e7341f15559
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-04 11:37:22 +0530

    Time: 0 ms (100%), Space: 9.8 MB (79.42%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,21 +1,37 @@
+//recursive
 class Solution {
 public:
+bool ispali(string &s , int i , int n){
+    if(i>=n) return true;
+    if(!isalnum(s[i])) return ispali(s,i+1,n);
+    if(!isalnum(s[n])) return ispali(s,i,n-1);
+    if(tolower(s[i]) != tolower(s[n])) return false;
+    return ispali(s,i+1,n-1);
+}
     bool isPalindrome(string s) {
-        int i=0;
-        int n=s.size()-1;
-        while(i<=n){
-            if(!isalnum(s[i])){
-                i++;
-                continue;
-            }
-            if(!isalnum(s[n])){
-                n--;
-                continue;
-            }
-            if(tolower(s[i])!=tolower(s[n])) return false;
-            i++;
-            n--;
-        }
-        return true;
+        int n= s.size()-1;
+        return ispali(s,0,n);
     }
 };
+//iterative
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         int i=0;
+//         int n=s.size()-1;
+//         while(i<=n){
+//             if(!isalnum(s[i])){
+//                 i++;
+//                 continue;
+//             }
+//             if(!isalnum(s[n])){
+//                 n--;
+//                 continue;
+//             }
+//             if(tolower(s[i])!=tolower(s[n])) return false;
+//             i++;
+//             n--;
+//         }
+//         return true;
+//     }
+// };
