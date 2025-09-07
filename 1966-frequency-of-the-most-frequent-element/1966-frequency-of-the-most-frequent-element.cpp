18d6e0101b03597cdc6a8f920706b8282e7773f7
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-07 17:25:04 +0530

    Time: 34 ms (79.13%), Space: 106 MB (29.76%) - LeetHub

--- 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
@@ -3,20 +3,20 @@ public:
     int maxFrequency(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());
         int l=0;
-        int r =0;
-        int n= nums.size();
+        int r=0;
         long long csum=0;
+        int n= nums.size();
         int result=-1;
         while(r<n){
-            long long target= nums[r];
             csum+=nums[r];
-            long long reqsum = target * (r-l+1);
-            long long ops= reqsum-csum;
-            if(ops>k){
+            long long target= nums[r];
+            long long reqsum= target*(r-l+1);
+            long long op= reqsum-csum;
+            if(op>k){
                 csum-=nums[l];
                 l++;
             }
-            result= max(result,(r-l+1));
+            result=max(result,(r-l+1));
             r++;
         }
         return result;
