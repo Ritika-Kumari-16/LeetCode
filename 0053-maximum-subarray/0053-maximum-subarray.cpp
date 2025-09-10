bd8713ca46cda2c2c0f1f44879365ade4e6a4a6e
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-10 15:05:58 +0530

    Time: 0 ms (100%), Space: 71.8 MB (52.03%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,20 +3,17 @@ public:
     int maxSubArray(vector<int>& nums) {
         int psum=0;
         int maxsum=-1;
-        int nsum=0;
         int maxnsum=INT_MIN;
         int n = nums.size();
         int l=0;
         while(l<n){
             if(psum+nums[l]>=0){
-                nsum=0;
                 psum+=nums[l];
                 maxsum=max(maxsum,psum);
             }
             else{
                 psum=0;
-                nsum=nums[l];
-                maxnsum=max(maxnsum,nsum);
+                maxnsum=max(maxnsum,nums[l]);
             }
             l++;
             
@@ -25,3 +22,23 @@ public:
         return maxnsum;
     }
 };
+
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+//         int n = nums.size()  ;
+//         vector<int>prefixSum(n+1) , minprefixSum(n+1) ;
+//         prefixSum[0] = 0 ;
+//         minprefixSum[0] =  0 ;
+//         int maxi = nums[0] ;
+//         for (int i = 1 ; i < n+1 ; i++){
+//             prefixSum[i] = prefixSum[i-1] + nums[i-1] ;
+//             minprefixSum[i] = min(minprefixSum[i-1] , prefixSum[i]) ;
+//             maxi = max(maxi , (prefixSum[i] - minprefixSum[i-1])) ;
+
+
+//         }
+
+//         return maxi ; 
+//     }
+// };
