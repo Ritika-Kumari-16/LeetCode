39c80bd9dd2f6aad0b16ca8204da1759ecb91eb3
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-11 17:02:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& v) {
-	int cnt1=0,cnt2=0;
-	int ele1=INT_MIN, ele2=INT_MIN;
-	for(int i=0;i<v.size();i++){
-		if(cnt1==0&&ele2!=v[i]) {
-			cnt1=1;
-			ele1=v[i];
-		}
-		else if( cnt2==0&&ele1!=v[i]){
-			cnt2=1;
-			ele2=v[i];
-		}
-		else if( ele1==v[i]) cnt1++;
-		else if( ele2==v[i]) cnt2++;
-		else {cnt1--;cnt2--;}
-	}
-	int m=v.size()/3;
-	cnt1=0;cnt2=0;
-	for(int i=0;i<v.size();i++){
-		if(ele1==v[i]) cnt1++;
-		if(ele2==v[i]) cnt2++;
-	}
-	vector<int>ans;
-	if(cnt1>m) ans.push_back(ele1);
-	if(cnt2>m) ans.push_back(ele2);
-    return ans;
-        
+    vector<int> majorityElement(vector<int>& nums) {
+        int maxel1=-1;
+        int maxel2=-1;
+        int cnt1=0 , cnt2=0;
+        for(int i=0;i<nums.size();i++){
+            if(cnt1==0 && nums[i]!=maxel2){
+                maxel1=nums[i];
+                cnt1++;
+            }
+            else if(cnt2==0 && maxel1!=nums[i]){
+                maxel2=nums[i];
+                cnt2++;
+            }
+            else if(nums[i]==maxel1) cnt1++;
+            else if(nums[i]==maxel2) cnt2++;
+            else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+        cnt1=0;
+        cnt2=0;
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==maxel1) cnt1++;
+            if(nums[i]==maxel2) cnt2++;
+        }
+        if(cnt1>nums.size()/3) ans.push_back(maxel1);
+        if(cnt2>nums.size()/3) ans.push_back(maxel2);
+        return ans;
     }
 };
