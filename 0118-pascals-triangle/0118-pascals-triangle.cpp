bad043cbcb401f0a6a9cdbcd9cd2caec685dcd0c
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-11 16:44:53 +0530

    Time: 0 ms (100%), Space: 9.7 MB (64.43%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> a(numRows);
-        for(int i=0; i<numRows; i++){
-            a[i].assign(i+1, 1);// exact allocation once
-            for(int&& j=1; j<=i/2; j++){
-                  a[i][i-j]=a[i][j]=a[i-1][j-1]+a[i-1][j];
-                        
-            }              
+        vector<vector<int>>ans;
+        for(int i=1;i<=numRows; i++){
+            vector<int>temp;
+            temp.push_back(1);
+            int val=1;
+            for(int j=1;j<i;j++){
+                val=val*(i-j);
+                val=val/j;
+                temp.push_back(val);
+            }
+            ans.push_back(temp);
         }
-        return a;
+        return ans;
     }
 };
