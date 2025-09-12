a818b7ec035c4a9ddc4f247c988a0294d5360713
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-12 14:15:52 +0530

    Time: 0 ms (100%), Space: 31.4 MB (45.35%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
+int recsearch(vector<int>&nums, int low , int high , int target){
+    if(low>high) return -1;
+    int mid= low+(high-low)/2;
+    if(nums[mid]==target) return mid;
+    else if(nums[mid]>target) return recsearch(nums,0,mid-1,target);
+    else return recsearch(nums,mid+1,high,target);
+}
     int search(vector<int>& nums, int target) {
         int n= nums.size();
-        int low=0;
-        int high=n-1;
-        while(low<=high){
-            int mid= low+(high-low)/2;
-            if(nums[mid]==target) return mid;
-            else if(nums[mid]>target) high=mid-1;
-            else low=mid+1;
-        }
-        return -1;
+        return recsearch(nums,0,n-1 , target);
+        
     }
 };
