97d9f984d7b5330f6dfc1005dc03c3a2ea334f6b
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-12 18:27:22 +0530

    Time: 5 ms (67.72%), Space: 19.1 MB (53.73%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+bool isvowel(char c){
+    return (c=='a' || c=='e' || c=='i' || c=='o' || c=='u');
+}
+    bool doesAliceWin(string s) {
+        for(int i=0;i<s.size();i++){
+            if(isvowel(s[i])) return true;
+        }
+        return false;
+    }
+};
