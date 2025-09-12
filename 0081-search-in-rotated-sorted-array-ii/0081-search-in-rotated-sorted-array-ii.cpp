df3dd397bb1c104df7bd4f7500e1c4301f7221a7
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-12 15:56:45 +0530

    Time: 0 ms (100%), Space: 17.9 MB (89.54%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int low = 0;
-        int high = nums.size() - 1;
-        while (low <= high) {
-            int mid = (low + (high - low) / 2);
-            if (nums[mid] == target) {
-                return true;
-            }
-            if(nums[low]==nums[mid]&&nums[mid]==nums[high]){
+        int n= nums.size();
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target) return true;
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
                 low++;
                 high--;
                 continue;
             }
-            else if (nums[low] <= nums[mid] ){
-                if(nums[low] <= target && nums[mid] >= target){
-                high = mid - 1;}
-                else {
-                low = mid + 1;
-            }}
+            if(nums[low]<=nums[mid]){
+                if(target>=nums[low] && target<nums[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
             else{
-                if(nums[mid]<=target&&nums[high]>=target){
+                if(target>nums[mid] && target<=nums[high]){
                     low=mid+1;
                 }
                 else{
