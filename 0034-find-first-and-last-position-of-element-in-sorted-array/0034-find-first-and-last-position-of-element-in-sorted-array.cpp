892330b63cd6798398f0a999d0ae76d883f2dcdb
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-12 14:59:44 +0530

    Time: 0 ms (100%), Space: 17.5 MB (86.43%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,17 +1,51 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
+int lowerBound(vector<int> &nums, int x){
         int n=nums.size();
-        vector<int>ans;
-        int lb=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-        int ub=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
-        if(lb==n||nums[lb]!=target){
-            ans.push_back(-1);
-            ans.push_back(-1);
-            return ans;
+        int low=0;
+        int high=n-1;
+        int idx=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==x){
+                idx=mid;
+                high=mid-1;
+            }
+            else if(nums[mid]>x){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+
         }
-        ans.push_back(lb);
-        ans.push_back(ub-1);
-        return ans;
+        return idx;
+    }
+    int upperBound(vector<int> &nums, int x){
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        int idx=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==x){
+                idx=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]<x){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;;
+            }
+
+        }
+        return idx;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int firstocc= lowerBound(nums,target);
+        int lastocc= upperBound(nums,target);
+        return {firstocc,lastocc};
+        
     }
 };
