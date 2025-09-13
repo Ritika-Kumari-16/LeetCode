37494998dd0df68917bb830a92f0d109da99c0ca
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-14 01:04:39 +0530

    Time: 0 ms (100%), Space: 50.5 MB (29.49%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
-    int maxelincol(vector<vector<int>>& mat , int col){
-        int maxel=-1;
-        int idx=-1;
-        for(int i=0;i<mat.size();i++){
-            if(mat[i][col]>maxel){
-                maxel=mat[i][col];
-                idx=i;
-            }
-        }
-        return idx;
-    }
-    vector<int> findPeakGrid(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int n= mat.size();
+        int m= mat[0].size();
         int low=0;
         int high=m-1;
         while(low<=high){
-            int mid=low+(high-low)/2;
-            int maxelindex=maxelincol(matrix,mid);
-            int left=-1,right=-1;
-            if(mid-1>=0) left=matrix[maxelindex][mid-1];
-            if(mid+1<m) right=matrix[maxelindex][mid+1];
-            if(left<matrix[maxelindex][mid] && matrix[maxelindex][mid] > right){
-                return {maxelindex,mid};
+            int mid= low+(high-low)/2;
+            int maxel=INT_MIN;
+            int idx=-1;
+            for(int i=0;i<n;i++){
+                if(maxel<mat[i][mid]){
+                    maxel=mat[i][mid];
+                    idx=i;
+                }
             }
-            else if(matrix[maxelindex][mid]<left){
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
+            int prev= mid==0 ? -1 : mat[idx][mid-1];
+            int next=mid==m-1? -1 : mat[idx][mid+1];
+            if(maxel > prev && maxel > next) return {idx,mid};
+            if(maxel > prev) low=mid+1;
+            else high=mid-1;
         }
         return {-1,-1};
-        
     }
 };
