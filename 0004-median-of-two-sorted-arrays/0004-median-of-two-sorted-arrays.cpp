e1a1f69a35cd4eec6d13eb7d398ab6185e4bc5ed
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-13 22:55:08 +0530

    Time: 0 ms (100%), Space: 95 MB (93.74%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,47 +1,32 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
+        int n= nums1.size();
         int m= nums2.size();
-        if(n>m) return findMedianSortedArrays(nums2,nums1);
-
-        int noofele= (n+m+1)/2;
+        if(n>m) return findMedianSortedArrays(nums2 , nums1);
+        int k= (n+m+1)/2;
         int low=0;
         int high=n;
         while(low<=high){
-            int mid= low+(high-low)/2;
-            int remel=noofele-mid;
-            int l1=INT_MIN,l2=INT_MIN;
-            int r1=INT_MAX , r2 = INT_MAX;
-            if(mid-1>=0){
-                l1=nums1[mid-1];
-            }
-            if(mid<n){
-                r1=nums1[mid];
-            }
-            if(remel-1>=0){
-                l2=nums2[remel-1];
-            }
-            if(remel<m){
-                r2=nums2[remel];
-            }
-            if(l1<=r2 && l2<=r1){
-                int left= max(l1,l2);
-                int right=min(r1,r2);
-                if((n+m)%2 ==0 ){
-                    return (double)(left+right)/ (double )2;
-                }
+            int mid=low+(high-low)/2;
+            int left1=INT_MIN , left2=INT_MIN;
+            int right1=INT_MAX , right2=INT_MAX;
+            if(mid>0) left1=nums1[mid-1];
+            if(mid<n) right1=nums1[mid];
+            int reqnos= k-mid;
+            if(reqnos>0) left2=nums2[reqnos-1];
+            if(reqnos<m) right2=nums2[reqnos]; 
+            if(left1<=right2 && left2<=right1){
+                if((n+m)%2==1) return (double)max(left1,left2);
                 else{
-                    return left;
+                    double ans= (double)(max(left1,left2)+min(right1,right2))/2;
+                    return ans;
                 }
             }
-            else if(l1>r2){
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
+            else if(left1>right2) high=mid-1;
+            else low=mid+1;
         }
-        return 0;
+        return -1;
+        
     }
 };
