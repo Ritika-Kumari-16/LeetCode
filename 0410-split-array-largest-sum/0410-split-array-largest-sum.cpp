fef9a904bce4b269b926fa7b0480edef344441ba
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-13 14:10:09 +0530

    Time: 0 ms (100%), Space: 10.4 MB (88.16%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,34 +1,37 @@
-int maxsubarr(vector<int>&arr,int mid){
-    int splits=1;
-    int summ=0;
-    for(int i=0;i<arr.size();i++){
-        if(summ+arr[i]<=mid){
-            summ+=arr[i];
-        }
-        else{
-            splits++;
-            summ=arr[i];
-
-        }
-    }
-    return splits;
-}
 class Solution {
 public:
+int countsubarr(vector<int>&nums, int sum){
+    int count=1;
+    int totalsum=0;
+    for(int i=0;i<nums.size();i++){
+        if(totalsum+nums[i]<=sum){
+            totalsum+=nums[i];
+        }
+        else{
+            totalsum=nums[i];
+            count++;
+        }
+    }
+    return count;
+}
     int splitArray(vector<int>& nums, int k) {
+        int n = nums.size();
         int low=*max_element(nums.begin(),nums.end());
-        int high=accumulate(nums.begin(),nums.end(),0);
+        int high= accumulate(nums.begin(),nums.end(),0LL);
+        int result=-1;
+        if(k>n) return result;
         while(low<=high){
             int mid=low+(high-low)/2;
-            int maxarr=maxsubarr(nums,mid);
-            if(maxarr>k){
-                low=mid+1;
-            }
-            else{
+            int totalsubarr= countsubarr(nums,mid);
+            if(totalsubarr<=k){
+                result=mid;
                 high=mid-1;
             }
-
+            else{
+                low=mid+1;
+            }
         }
-        return low;
+        return result;
+        
     }
 };
