be7d81bc706180efb3697920e68f64046f59635f
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-15 14:53:53 +0530

    Time: 3 ms (74.96%), Space: 16 MB (28.72%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -41,48 +41,40 @@ public:
         }
         return total;
     }
-    void nseidx(vector<int>&arr , vector<int>&nse ){
-        int n = arr.size();
-        stack<int>st;
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && arr[st.top()] >arr[i]){
-                st.pop();
-            }
-            if(!st.empty()){
-                nse[i]=st.top();
-            }
-            st.push(i);
-        }
+void findpse(vector<int>& arr , vector<int>&pse){
+    stack<int>st;
+    for(int i=0;i<arr.size();i++){
+        while(!st.empty() && arr[st.top()]>arr[i]) st.pop();
+        if(!st.empty()) pse[i]=st.top();
+        st.push(i);
     }
-    void pseidx(vector<int>&arr , vector<int>&pse ){
-        int n = arr.size();
-        stack<int>st;
-        for(int i=0;i<n;i++){
-            while(!st.empty() && arr[st.top()] >=arr[i]){
-                st.pop();
-            }
-            if(!st.empty()){
-                pse[i]=st.top();
-            }
-            st.push(i);
-        }
+}
+void findnse(vector<int>&arr , vector<int>&nse){
+    stack<int>st;
+    for(int i=arr.size()-1;i>=0;i--){
+        while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
+        if(!st.empty()) nse[i]=st.top();
+        st.push(i);
     }
+}
+
     long long sumSubarrayMins(vector<int>& arr) {
-        int n = arr.size();
-        long long total=0;
-        vector<int>nse(n,n);
-        vector<int>pse(n,-1);
-        nseidx(arr,nse);
-        pseidx(arr,pse);
-        for(int i=0;i<n;i++){
-            int el= arr[i];
-            int left=i-pse[i];
-            int right= nse[i]-i;
-            total+=(left*right*1ll * el);
+            int n= arr.size();
+            vector<int>pse(n,-1);
+            findpse(arr,pse);
+            vector<int>nse(n,n);
+            findnse(arr,nse);
+            long long totalsum=0;
+            for(int i=0;i<n;i++){
+                int el=arr[i];
+                int left=i-pse[i];
+                int right=nse[i]-i;
+                long long totalsubarray= (1LL*left*right);
+                totalsum=(totalsum+ (1LL* totalsubarray*el));
+            }
+            return totalsum;
         }
-        return total;
-    }
     long long subArrayRanges(vector<int>& nums) {
-        return sumSubarrayMaxs(nums) - sumSubarrayMins(nums);
+        return sumSubarrayMaxs(nums)-sumSubarrayMins(nums);
     }
 };
