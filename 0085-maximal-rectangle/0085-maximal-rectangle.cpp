5ccfc941837de6e43912751911ca2f73959daba8
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-15 16:06:29 +0530

    Time: 6 ms (66.71%), Space: 19.2 MB (46.09%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -2,48 +2,59 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        int maxarea=0;
         stack<int>st;
+        long long maxarea=0;
         for(int i=0;i<n;i++){
-            while(!st.empty() && heights[st.top()]> heights[i]){
-                int nsidx=i;
-                int el= heights[st.top()];
+            while(!st.empty() && heights[st.top()]>heights[i]){
+                int el=heights[st.top()];
                 st.pop();
-                int psidx= st.empty()?-1:st.top();
-                maxarea=max(maxarea,(nsidx-psidx-1)*el);
+                int left= st.empty() ? -1: st.top();
+                int maxlen=(i-left)-1;
+                long long area=maxlen*el;
+                maxarea=max(maxarea,area);
             }
             st.push(i);
         }
         while(!st.empty()){
-                int nsidx=n;
-                int el= heights[st.top()];
-                st.pop();
-                int psidx= st.empty()?-1:st.top();
-                maxarea=max(maxarea,(nsidx-psidx-1)*el);
-            }
-            return maxarea;
+            int right= n;
+            int el=heights[st.top()];
+            st.pop();
+            int left= st.empty()?-1:st.top();
+            int maxlen= (right-left)-1;
+            long long area=maxlen*el;
+            maxarea=max(maxarea,area);
+        }
+        return maxarea;
     }
+
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<vector<int>>prefix(n,vector<int>(m,0));
-        for(int i=0;i<m;i++){
-            prefix[0][i]=matrix[0][i]-'0';
+        int n= matrix.size();
+        int m= matrix[0].size();
+       vector<vector<int>> prefixheight(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]=='0'){
+                    prefixheight[i][j]=0;
+                }
+                else{
+                    prefixheight[i][j]=1;
+                }
+            }
         }
         for(int i=1;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]=='0'){
-                    prefix[i][j]=0;
+                    prefixheight[i][j]=0;
                 }
                 else{
-                    prefix[i][j]=prefix[i-1][j]+1;
+                    prefixheight[i][j]=prefixheight[i-1][j]+1;
                 }
             }
         }
-        int maxarea=0;
+        int maxarearec=0;
         for(int i=0;i<n;i++){
-            maxarea=max(maxarea,largestRectangleArea(prefix[i]));
+            maxarearec=max(maxarearec,largestRectangleArea(prefixheight[i]));
         }
-        return maxarea;
+        return maxarearec;
     }
 };
