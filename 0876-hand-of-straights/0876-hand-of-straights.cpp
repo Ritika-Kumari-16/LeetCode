a6112b68507f1390e3cd5ccab62dd555a9b36994
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-17 00:47:07 +0530

    Time: 44 ms (31.05%), Space: 32.4 MB (37.86%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.cpp
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        int n = hand.size();
-        if(n%groupSize) return false;
+        int n= hand.size();
+        if((n%groupSize)!=0) return false;
         map<int,int>mpp;
-        for(auto &it:hand){
+        for(auto it:hand){
             mpp[it]++;
         }
         while(!mpp.empty()){
-            int cur= mpp.begin()->first;
-            for(int i=0;i<groupSize;i++){
-                if(mpp.find(cur+i)==mpp.end()) return false;
-                mpp[cur+i]--;
-                if(mpp[cur+i]==0) mpp.erase(cur+i);
+            int el=mpp.begin()->first;
+            mpp[el]--;
+            if(mpp[el]==0) mpp.erase(el);
+            for(int i=1;i<groupSize;i++){
+                if(mpp.find(el+1)==mpp.end()) return false;
+                mpp[el+1]--;
+                if(mpp[el+1]==0) mpp.erase(el+1);
+                el++;
             }
         }
         return true;
