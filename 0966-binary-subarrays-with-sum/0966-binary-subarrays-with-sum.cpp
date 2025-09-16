5b983be8220f140598c77420e84937fdefcbb8b6
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-16 14:05:46 +0530

    Time: 0 ms (100%), Space: 32.7 MB (59.17%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int noofsubarr(vector<int>&nums , int goal){
-        if(goal<0) return 0;
-        int count=0;
-        int l=0,r=0;
-        int sum=0;
-        int n = nums.size();
-        while(r<n){
-            sum+=nums[r];
-            while(sum>goal){
-                sum-=nums[l];
-                l++;
-            }
-            if(sum<=goal){
-                count+=r-l+1;
-            }
-            r++;
+int noofsub(vector<int>&nums , int goal){
+    int count=0;
+    if(goal<0) return count;
+    int l=0;
+    int r=0;
+    int n= nums.size();
+    int sum=0;
+    while(r<n){
+        sum+=nums[r];
+        while(sum>goal){
+            sum-=nums[l];
+            l++;
         }
-        return count;
+        if(sum<=goal){
+            count+=(r-l+1);
+        }
+        r++;
     }
+    return count;
+}
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return noofsubarr(nums,goal)-noofsubarr(nums,goal-1);
-        
+        return noofsub(nums,goal)-noofsub(nums,goal-1);
     }
 };
