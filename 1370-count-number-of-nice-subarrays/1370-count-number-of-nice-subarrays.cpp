392b5fdf275d70c26ca40cb65d0c16f867ef306e
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-16 14:24:55 +0530

    Time: 9 ms (49.52%), Space: 71.4 MB (68.26%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    int countarr(vector<int>&nums , int k){
-        if(k<0) return 0;
-        int l=0,r=0,count=0;
-        int n = nums.size();
-        int oddcnt=0;
-        while(r<n){
-            if(nums[r]%2==1){
-                oddcnt++;
-            }
-            while(oddcnt>k){
-                if(nums[l]%2==1){
-                    oddcnt--;
-                }
-                l++;
-            }
-            if(oddcnt<=k){
-                count+=r-l+1;
-            }
-            r++;
+int noofsub(vector<int>&nums , int goal){
+    int count=0;
+    if(goal<0) return count;
+    int l=0;
+    int r=0;
+    int n= nums.size();
+    int sum=0;
+    while(r<n){
+        sum+=nums[r]%2;
+        while(sum>goal){
+            sum-=nums[l]%2;
+            l++;
         }
-        return count;
+        if(sum<=goal){
+            count+=(r-l+1);
+        }
+        r++;
     }
+    return count;
+}
     int numberOfSubarrays(vector<int>& nums, int k) {
-        return countarr(nums,k)-countarr(nums,k-1);
-        
+        return noofsub(nums,k)-noofsub(nums,k-1);
     }
 };
