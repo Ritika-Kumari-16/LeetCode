2345929a04be1217ebe7e3710dd5d5d9cf2b4f64
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-16 16:11:44 +0530

    Time: 90 ms (37.32%), Space: 56.3 MB (43.32%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-int countsub(vector<int>&nums, int k){
+int countsubarray(vector<int>&nums , int k){
+    unordered_map<int,int>mpp;
+    int n= nums.size();
     if(k<0) return 0;
-        int l=0,r=0;
-        int n= nums.size();
-        int count=0;
-        unordered_map<int,int>mpp;
-        while(r<n){
-            mpp[nums[r]]++;
-            while(mpp.size()>k){
-                mpp[nums[l]]--;
-                if(mpp[nums[l]]==0){
-                    mpp.erase(nums[l]);
-                }
-                l++;
-            }
-            if(mpp.size()<=k){
-                count+=r-l+1;
-            }
-            r++;
+    int count=0;
+    int l=0,r=0;
+    while(r<n){
+        mpp[nums[r]]++;
+        while(mpp.size()>k){
+            mpp[nums[l]]--;
+            if(mpp[nums[l]]==0) mpp.erase(nums[l]);
+            l++;
         }
-        return count;
+        count+=(r-l+1);
+        r++;
+    }
+    return count;
 }
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-
-        return countsub(nums,k)-countsub(nums,k-1);
+        return countsubarray(nums,k)-countsubarray(nums,k-1);
+        
     }
 };
