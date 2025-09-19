ff07f5263c9d077aeb8c37d0a202178ca38e4872
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-20 01:36:59 +0530

    Time: 0 ms (100%), Space: 52.2 MB (66.15%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
-        int maxidx=0;
-        int jump=0;
+        int n= nums.size();
+        int maxidx=nums[0];
         for(int i=0;i<n;i++){
             if(maxidx<i) return false;
-            jump=nums[i]+i;
-            maxidx=max(maxidx,jump);
+            maxidx=max(maxidx,i+nums[i]);
         }
         return true;
     }
