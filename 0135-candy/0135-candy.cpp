3aaf35b83fe6d8f844c9d8e65d114e1d8a154904
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-21 18:06:18 +0530

    Time: 0 ms (100%), Space: 22 MB (95.61%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n= ratings.size();
-        if(n==1) return 1;
-        vector<int>left(n,0);
-        left[0]=1;
-        for(int i=1;i<n;i++){
-            if(ratings[i]>ratings[i-1]){
-                left[i]=left[i-1]+1;
+        int totalcandies=1;
+        int i=1;
+        while(i<n){
+            while(i<n && ratings[i-1]==ratings[i]){
+                totalcandies++;
+                i++;
             }
-            else{
-                left[i]=1;
+            int peak=1;
+            while(i<n && ratings[i-1]<ratings[i]){
+                peak+=1;
+                totalcandies+=peak;
+                i++;
+            }
+            int down=1;
+            while(i<n && ratings[i-1]>ratings[i]){
+                totalcandies+=down;
+                down++;
+                i++;
+            }
+            if(down>peak){
+                totalcandies+=(down-peak);
             }
         }
-        int rightval=1;
-        int maxcandies=0;
-        maxcandies+=(max(rightval,left[n-1]));
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                rightval+=1;
-                
-            }
-            else{
-                rightval=1;
-            }
-            maxcandies+=max(rightval,left[i]);
-        }
-        return maxcandies;
+        return totalcandies;
+        
     }
 };
