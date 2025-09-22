dee9e275ffda3cf5091d63657139b3127f999dfa
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-22 13:39:54 +0530

    Time: 189 ms (13.04%), Space: 213.6 MB (6.55%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -1,54 +1,108 @@
+struct Node{
+    int key;
+    int val;
+    int count;
+    Node* prev;
+    Node* next;
+    Node(int k , int v){
+        key=k;
+        val=v;
+        count=1;
+        prev=NULL;
+        next=NULL;
+    }
+};
+struct List{
+    Node* head;
+    Node* tail;
+    int size;
+    List(){
+        head= new Node(-1,-1);
+        tail=new Node(-1,-1);
+        head->next=tail;
+        tail->prev=head;
+        size=0;
+    }
+    void insertnode(Node* node){
+        Node* prevnode= tail->prev;
+        prevnode->next=node;
+        node->prev=prevnode;
+        node->next=tail;
+        tail->prev=node;
+        size++;
+    }
+    void removenode(Node* node){
+        Node* prevnode=node->prev;
+        Node* nextnode=node->next;
+        prevnode->next=nextnode;
+        nextnode->prev=prevnode;
+        size--;
+    }
+};
 class LFUCache {
-    int capacity;
-    int minFreq;
-    unordered_map<int,pair<int,int>> keyVal;
-    unordered_map<int,list<int>> freqList;
-    unordered_map<int,list<int>::iterator> pos;
 public:
+map<int,List*>freqlist;
+map<int,Node*>keynode;
+int cap;
+int cursize;
+int minfreq;
+
+void updatefreq(Node* node){
+    keynode.erase(node->key);
+    freqlist[node->count]->removenode(node);
+    if(node->count==minfreq && freqlist[node->count]->size==0){
+        minfreq++;
+    }
+    List* newlist= new List();
+    if(freqlist.find(node->count+1)!=freqlist.end()){
+        newlist=freqlist[node->count+1];
+    }
+    node->count+=1;
+    newlist->insertnode(node);
+    freqlist[node->count]=newlist;
+    keynode[node->key]=node;
+}
     LFUCache(int capacity) {
-        this->capacity = capacity;
-        minFreq = 0;
+        cap=capacity;
+        cursize=0;
+        minfreq=0;
     }
     
     int get(int key) {
-        if(keyVal.find(key) == keyVal.end())
-            return -1;
-        freqList[keyVal[key].second].erase(pos[key]);
-        keyVal[key].second++;
-        freqList[keyVal[key].second].push_back(key);
-        pos[key] = --freqList[keyVal[key].second].end();
-        if(freqList[minFreq].empty())
-            minFreq++;
-        return keyVal[key].first;
+        if(keynode.find(key)==keynode.end()) return -1;
+        Node* node= keynode[key];
+        int value= node->val;
+        updatefreq(node);
+        return value;
     }
     
     void put(int key, int value) {
-        if(!capacity)
-            return;
-        if(keyVal.find(key) != keyVal.end()) {
-            keyVal[key].first = value;
-            freqList[keyVal[key].second].erase(pos[key]);
-            keyVal[key].second++;
-            freqList[keyVal[key].second].push_back(key);
-            pos[key] = --freqList[keyVal[key].second].end();
-            if(freqList[minFreq].empty())
-                minFreq++;
-            return;
+        if(cap==0) return ;
+        if(keynode.find(key)!=keynode.end()){
+            Node* node= keynode[key];
+            node->val=value;
+            updatefreq(node);
         }
-        if(keyVal.size() == capacity) {
-            int delKey = freqList[minFreq].front();
-            keyVal.erase(delKey);
-            pos.erase(delKey);
-            freqList[minFreq].pop_front();
+        else{
+            if(cursize==cap){
+                List* temp=freqlist[minfreq];
+                keynode.erase(temp->head->next->key);
+                freqlist[minfreq]->removenode(temp->head->next);
+                cursize--;
+            }
+            cursize++;
+            minfreq=1;
+            List* newlist= new List();
+            if(freqlist.find(minfreq)!=freqlist.end()){
+                newlist=freqlist[minfreq];
+            }
+            Node* node= new Node(key,value);
+            keynode[key]=node;
+            newlist->insertnode(node);
+            freqlist[minfreq]=newlist;
         }
-        keyVal[key] = {value,1};
-        freqList[1].push_back(key);
-        pos[key] = --freqList[1].end();
-        minFreq = 1;
     }
 };
-
-
 
 /**
  * Your LFUCache object will be instantiated and called as such:
