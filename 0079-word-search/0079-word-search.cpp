edbf4cae0cd7f463495abb55dc884159a6334808
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-24 00:53:47 +0530

    Time: 210 ms (74.04%), Space: 10.7 MB (58.86%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    bool solve(int row , int col , int i , vector<vector<char>>&board , string &word , vector<vector<int>>&visited){
-        if(i==word.size()) return true;
-        int n = board.size();
-        int m= board[0].size();
-        if(row<0 || row>=n || col<0 || col>=m || visited[row][col]==1 ||board[row][col]!=word[i] ) return false;
-        visited[row][col]=1;
-        if (solve(row-1,col,i+1,board,word,visited) 
-                || solve(row,col-1,i+1,board,word,visited) 
-                || solve(row,col+1,i+1,board,word,visited) 
-                || solve(row+1,col,i+1,board,word,visited) ){
-                    return true;
-                }
-        visited[row][col]=-1;
-        return false;
+bool dfs(int i , int j , int k,string &word , vector<vector<char>>&board , vector<vector<int>>&visited){
+    int n= board.size();
+    int m=board[0].size();
+    if(k==word.size()) return true;
+    if(i < 0 || i>=n || j<0 ||j>=m || word[k]!=board[i][j] || visited[i][j]==1 ) return false;
+    visited[i][j]=1;
+    int drow[]={-1,0,0,1};
+    int dcol[]={0,-1,1,0};
+    for(int a=0;a<4;a++){
+        int nrow=i+drow[a];
+        int ncol=j+dcol[a];
+        if(dfs(nrow,ncol,k+1,word,board,visited)) return true;
     }
+    visited[i][j]=-1;
+    return false;
+}
     bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size();
-        int m= board[0].size();
+        int n= board.size();
+        int m=board[0].size();
         vector<vector<int>>visited(n,vector<int>(m,-1));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(board[i][j]==word[0]){
-                    if(solve(i,j,0,board,word,visited)) return true;
+                    if(dfs(i,j,0,word,board,visited)) return true;
                 }
             }
         }
