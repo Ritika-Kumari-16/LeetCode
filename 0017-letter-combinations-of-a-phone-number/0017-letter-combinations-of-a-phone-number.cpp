6195a9a300575119150e49aaee7d6c8adb90898a
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-23 23:45:53 +0530

    Time: 0 ms (100%), Space: 9.3 MB (38.87%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    unordered_map<char, string> map {
-        {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"},
-        {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
-    };
-    void comb(string digits , int ind , string temp2, vector<string>&ans ){
-        if(ind==digits.size()) {
-            ans.push_back(temp2);
-
-            return ;}
-        char c= digits[ind];
-        string temp=map[c];
-        for(int i=0;i<temp.size();i++){
-            temp2+=temp[i];
-            comb(digits,ind+1,temp2,ans);
-            temp2.pop_back();
-        }
+unordered_map<char,string>mpp={
+    {'0',""},{'1',""},{'2' , "abc"} , {'3',"def"} , {'4',"ghi"} ,{'5',"jkl"},{'6',"mno"} ,{'7',"pqrs"}
+    ,{'8' ,"tuv"} ,{'9',"wxyz"}
+};
+void generatecomb(int i , int n , string &digits, string &temp , vector<string>&ans){
+    if(i==n){
+        ans.push_back(temp);
+        return;
     }
+    string s= mpp[digits[i]];
+    for(int j=0;j<s.size();j++){
+        temp+=s[j];
+        generatecomb(i+1,n,digits,temp, ans);
+        temp.pop_back();
+    }
+}
     vector<string> letterCombinations(string digits) {
+        int n= digits.size();
+        if(n==0) return {};
         vector<string>ans;
-        if(digits.empty()) 
-        {return ans;}
-        string temp2="";
-        comb(digits , 0 , temp2,ans);
+        string temp;
+        generatecomb(0,n,digits,temp,ans);
         return ans;
     }
 };
