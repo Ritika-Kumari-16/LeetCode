f3c633e14a0ab354d033b986865d35258c05c407
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-23 17:23:16 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
+double findans(double x , long long n){
+    if(n==0) return 1;
+    if(n%2==0 ) {
+        return findans(x*x , n/2);
+    }
+    return x * findans(x,n-1);
+}
     double myPow(double x, int n) {
-        long long m=n;
+        long long m= n;
         if(n<0){
             m=m*-1;
         }
-        double ans=1;
-        while(m>0){
-            if(m%2==0){
-                x=x*x;
-                m=m/2;
-            }
-            else{
-                ans*=x;
-                m=m-1;
-            }
-        }
-        if(n<0) return (double) 1/ ans;
-        return ans;
+        double ans=findans(x,m);
+        return (n<0) ? 1/ans : ans;
     }
 };
+
+// class Solution {
+// public:
+//     double myPow(double x, int n) {
+//         long long m=n;
+//         if(n<0){
+//             m=m*-1;
+//         }
+//         double ans=1;
+//         while(m>0){
+//             if(m%2==0){
+//                 x=x*x;
+//                 m=m/2;
+//             }
+//             else{
+//                 ans*=x;
+//                 m=m-1;
+//             }
+//         }
+//         if(n<0) return (double) 1/ ans;
+//         return ans;
+//     }
+// };
