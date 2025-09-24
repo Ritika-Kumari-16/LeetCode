d38785fcddb979ea431a0e983c63957cf0ffa201
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-25 03:20:07 +0530

    Time: 0 ms (100%), Space: 9.5 MB (36.85%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.cpp
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-    std::string fractionToDecimal(int numerator, int denominator) {
-        if (numerator == 0)
-            return "0";        
-
-        std::string fraction;
-        if ((numerator < 0) ^ (denominator < 0))
-            fraction += "-";        
-
-        long long dividend = std::llabs((long long)numerator);
-        long long divisor = std::llabs((long long)denominator);
-        fraction += std::to_string(dividend / divisor);
-        long long remainder = dividend % divisor;
-        if (remainder == 0) {
-            return fraction;
+    string fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) return "0";
+        string res;
+        if ((numerator < 0) ^ (denominator < 0)) res += "-";
+        long long x = abs((long long)numerator), y = abs((long long)denominator);
+        if (x % y == 0) return res + to_string(x / y);
+        else {
+            res += to_string(x / y);
+            x %= y;
+            res += '.';
         }
-
-        fraction += ".";
-        std::unordered_map<long long, int> map;
-        while (remainder != 0) {
-            if (map.count(remainder)) {
-                fraction.insert(map[remainder], "(");
-                fraction += ")";
-                break;
+        unordered_map<int, int> hash;
+        while (x) {
+            if (hash.count(x)) {
+                res.insert(hash[x], "(");
+                res += ')';
+                return res;
+            } else {
+                hash[x] = res.size();
+                x *= 10;
+                res += to_string(x / y);
+                x %= y;
             }
-            map[remainder] = fraction.size();
-            remainder *= 10;
-            fraction += std::to_string(remainder / divisor);
-            remainder %= divisor;
         }
-
-        return fraction;
+        return res;
     }
 };
