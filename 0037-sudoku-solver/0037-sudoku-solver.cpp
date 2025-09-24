292b98eb7ebc433c6f5fc00eb3b2e30eb423a8ce
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-24 10:59:52 +0530

    Time: 371 ms (45.91%), Space: 8.9 MB (10.13%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-bool valid(vector<vector<char>>& board ,int i , int j , char ch){
-    for(int id=0;id<9;id++){
-        if(board[id][j]==ch) return false;
-        if(board[i][id]==ch) return false;
-        if(board[3*(i/3)+id/3][3*(j/3)+id%3]==ch)return false;
+bool canplace(vector<vector<char>>&board , int row , int col, char c){
+    for(int i=0;i<9;i++){
+        if(board[row][i]==c) return false;
+        if(board[i][col]==c) return false;
+        if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c) return false;
     }
     return true;
 }
 bool solve(vector<vector<char>>&board){
-        int n =9;
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                if(board[i][j]=='.'){
-                    for(char ch='1';ch<='9';ch++){
-                        if(valid(board,i,j,ch)){
-                            board[i][j]=ch;
-                            if(solve(board)) return true;
-                            board[i][j]='.';
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            if(board[i][j]=='.'){
+                for(char c='1' ;c<='9';c++){
+                    if(canplace(board,i,j,c)){
+                        board[i][j]=c;
+                        if(solve(board)){
+                            return true;
                         }
+                        board[i][j]='.';
                     }
-                    return false;
                 }
+                return false;
             }
-        }
-        return true;
 
+        }
+    }
+    return true;
 }
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+        bool cansolve=solve(board);
     }
 };
