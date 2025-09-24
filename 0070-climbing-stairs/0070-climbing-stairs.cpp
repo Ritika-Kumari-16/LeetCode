cd4f2d7a77351e8e62bdc9e8c923e2d4c6fba81a
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-25 00:09:45 +0530

    Time: 0 ms (100%), Space: 8.4 MB (52.49%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
+int countways(int n , vector<int>&dp){
+    if(n==0) return 1;
+    if(n<0) return 0;
+    if(dp[n]!=-1) return dp[n];
+    int onesteps=countways(n-1,dp);
+    int twosteps=countways(n-2,dp);
+    return dp[n]=onesteps+twosteps;
+}
     int climbStairs(int n) {
-        vector<int>dp(n+2,0);
-        dp[n]=1;
-        for(int i=n-1;i>=0;i--){
-            dp[i]=dp[i+1]+dp[i+2];
-        }
-        return dp[0];
+        vector<int>dp(n+1,-1);
+        return countways(n,dp);
     }
 };
-
-// class Solution {
-// public:
-// int solve(int i , int n,vector<int>&dp){
-//     if(i==n) return 1;
-//     if(i>n) return 0;
-//     if(dp[i]!=-1) return dp[i];
-//     //one step 
-//     int onestep=solve(i+1,n,dp);
-//     int twosteps=solve(i+2,n,dp);
-//     return dp[i]=onestep+twosteps;
-// }
-//     int climbStairs(int n) {
-//         vector<int>dp(n+1,-1);
-//         return solve (0,n,dp);
-        
-//     }
-// };
