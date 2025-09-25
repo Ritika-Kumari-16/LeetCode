fe8f9b2c1a42876a536fdf3ec3ccab61aa1e5a08
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-25 09:52:12 +0530

    Time: 0 ms (100%), Space: 9.4 MB (33.96%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
+int possibleways(int i , int j, int m , int n){
+    if(i==m-1 && j==n-1) return 1;
+    if(i>=m || j>=n) return 0;
+    int right= possibleways(i,j+1,m,n);
+    int down =possibleways(i+1,j,m,n);
+    return right+down;
+}
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,0));
+        // return possibleways(0,0,m,n);
+        vector<vector<int>>dp(m , vector<int>(n,0));
         dp[m-1][n-1]=1;
         for(int i=m-1;i>=0;i--){
             for(int j=n-1;j>=0;j--){
-                if (i == m - 1 && j == n - 1) continue;
-                int up =0;
-                if(i+1<=m-1){
-                    up=dp[i+1][j];
+                if(i==m-1 && j==n-1) continue;
+                int right=0;
+                if(j+1<n){
+                    right=dp[i][j+1];
                 }
-                int left=0;
-                if(j+1<=n-1){
-                    left=dp[i][j+1];
+                int down=0;
+                if(i+1<m){
+                    down=dp[i+1][j];
                 }
-                dp[i][j]=up+left;
+                dp[i][j]=right+down;
             }
         }
         return dp[0][0];
     }
 };
-
-// class Solution {
-// public:
-// int solve( int i , int j , int n , int m , vector<vector<int>>&dp){
-//     if(i==n-1 && j==m-1) return 1;
-//     if(i>=n || j>=m) return 0;
-//     if(dp[i][j]!=-1) return dp[i][j];
-//     int right= solve(i,j+1,n,m,dp);
-//     int down= solve(i+1,j,n,m,dp);
-//     return dp[i][j]=right+down;
-// }
-//     int uniquePaths(int m, int n) {
-//         vector<vector<int>>dp(m ,vector<int>(n,-1));
-//         return solve(0,0,m,n,dp); 
-//     }
-// };
