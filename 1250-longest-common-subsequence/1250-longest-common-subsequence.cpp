467f8fe15ffa71c69983f0539698e6b1949b9a08
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-29 01:39:01 +0530

    Time: 23 ms (81.4%), Space: 27.5 MB (29.74%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -15,7 +15,28 @@ int findlength(int i , int j , string &s1 , string &s2,vector<vector<int>>&dp){
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return findlength(0,0,text1,text2,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        // return findlength(0,0,text1,text2,dp);
+        // for(int i=0;i<n;i++){
+        //     if(text1[i]==text2[m-1]){
+        //         dp[i][m-1]=1;
+        //     }
+        // }
+        // for(int i=0;i<m;i++){
+        //     if(text2[i]==text1[n-1]){
+        //         dp[n-1][i]=1;
+        //     }
+        // }
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(text1[i]==text2[j]){
+                    dp[i][j]=1+dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j]= max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
