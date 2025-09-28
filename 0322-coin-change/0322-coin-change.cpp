7e295f77c7a3150505ea96ebe11c219dbb048f99
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-28 10:56:33 +0530

    Time: 63 ms (11.61%), Space: 49.8 MB (20.58%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-int solve(int i , int n, int amount, vector<int>& coins,vector<vector<int>>&dp){
+int noofcoins(int i , vector<int>&coins, int amount,vector<vector<int>>&dp){
+    int n= coins.size();
     if(i==n){
-        if(amount!=0) return 1e9;
-        return 0;
+        if(amount==0) return 0;
+        return 1e9;
     }
-    if(dp[i][amount]!=-1 ) return dp[i][amount];
-    //take 
-    int take =1e9;
+    if(dp[i][amount]!=-1) return dp[i][amount];
+    int take=1e9;
     if(coins[i]<=amount){
-        take=solve(i,n,amount-coins[i],coins,dp);
+        take=noofcoins(i,coins,amount-coins[i],dp);
     }
-    int nottake= solve(i+1,n,amount,coins,dp);
-    return dp[i][amount]= min(1+take , nottake);
+    int nottake= noofcoins(i+1,coins,amount,dp);
+    return dp[i][amount]=min(take+1,nottake);
 }
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
+        int n= coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int res=solve(0,n,amount,coins,dp);
-        return res>=1e9 ? -1:res;
-        
+        int ans=noofcoins(0,coins,amount,dp);
+        return (ans>=1e9)? -1 : ans;
     }
 };
