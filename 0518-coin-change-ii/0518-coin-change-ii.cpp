09aefff85f6d6ac48f1699cea127cdc2f0c38241
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-28 11:49:04 +0530

    Time: 55 ms (7.02%), Space: 109.8 MB (5.16%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+using u128 = unsigned __int128;
 int noofcoins(int i , vector<int>&coins, int amount,vector<vector<int>>&dp){
     int n= coins.size();
     if(i==n-1){
@@ -17,7 +18,21 @@ int noofcoins(int i , vector<int>&coins, int amount,vector<vector<int>>&dp){
 }
     int change(int amount, vector<int>& coins) {
         int n= coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return noofcoins(0,coins,amount,dp);
+        vector<vector<u128>>dp(n,vector<u128>(amount+1,0));
+        // return noofcoins(0,coins,amount,dp);
+        for(int target=0; target<=amount;target++){
+            if(target%coins[n-1]==0) dp[n-1][target]=1;
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int target=0;target<=amount;target++){
+                u128  take=0;
+                if(coins[i]<=target){
+                    take= dp[i][target-coins[i]];
+                }
+                u128 nottake=dp[i+1][target];
+                dp[i][target] = (take + nottake) ;
+            }
+        }
+        return (int)dp[0][amount];
     }
 };
