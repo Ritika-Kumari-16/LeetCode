e900d605878ff120c43e4820d622ad4a7b8eb5e2
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-28 15:46:23 +0530

    Time: 15 ms (53.52%), Space: 25.7 MB (27.97%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,46 +1,23 @@
 class Solution {
 public:
-int tabu(vector<int>&arr,int x){
-        int n=arr.size();
-        //  vector<vector<int>>dp(n,vector<int>(x+1,0));
-        vector<int>prev(x+1,0);
-        vector<int>curr(x+1,0);
-        if(arr[0]==0) prev[0]=2;
-        else prev[0]=1;
-        if(arr[0]!=0&&arr[0]<=x)prev[arr[0]]=1;
-        for(int ind=1;ind<n;ind++){
-            for(int sum=0;sum<=x;sum++){
-                int notTake=prev[sum];
-                int take=0;
-                if(arr[ind]<=sum) take=prev[sum-arr[ind]];
-                
-                
-                curr[sum]=(take+notTake);
-            }
-            prev=curr;
-        }
-        return prev[x];
+int countways(int i , int sum , vector<int>&nums, int target ,int sum1, vector<vector<int>>&dp){
+    if(i==nums.size()){
+        if(sum+sum1==target+sum1) return 1 ;
+        return 0;
     }
-    int countPartitions(int n, int d, vector<int>& arr) {
-        // Code here
-        //Approach -> Memoization
-        int tosum=0;
-        for(int i=0;i<n;i++){
-            tosum+=arr[i];
-        }
-        if(tosum-d<0||(tosum-d)%2)return 0;
-        int x=(tosum-d)/2;
-        // vector<vector<int>>dp(n,vector<int>(x+1,-1));
-        // return solve(n-1,d,arr,x,dp);
-        
-        
-        //Approach Tabulation
-        return tabu(arr,x);
-       
-        
-        
-    }
+    if(dp[i][sum+sum1]!=-1) return dp[i][sum+sum1];
+    int addways= countways(i+1,sum+nums[i],nums,target,sum1,dp);
+    int subways= countways(i+1,sum-nums[i],nums,target,sum1,dp);
+    return dp[i][sum+sum1]=addways+subways;
+}
     int findTargetSumWays(vector<int>& nums, int target) {
-        return countPartitions(nums.size(),target,nums);
+        int n= nums.size();
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        // if(sum-target <0 || (sum-target)%2){
+        //     return 0;
+        // }
+        // int s2= (sum-target)/2;
+        vector<vector<int>>dp(n,vector<int>(2*sum+1,-1));
+        return countways(0,0,nums,target,sum,dp);
     }
 };
