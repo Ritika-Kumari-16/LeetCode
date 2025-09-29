02d4872c96fe8a3115f608b3824489c2874edbb4
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-29 14:52:12 +0530

    Time: 3 ms (90.64%), Space: 13.3 MB (30.48%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,21 +1,46 @@
 class Solution {
 public:
-    int solve(int n , int m , string &w1, string &w2,vector<vector<int>>&dp){
-        if(n<0&&m<0){return 0;}
-        if(n<0) return m+1;
-        if(m<0) return n+1;
-        if(dp[n][m]!=-1) return dp[n][m];
-        if(w1[n]==w2[m]) {
-            return 0 + solve(n-1,m-1,w1,w2,dp);
-        }
-        return dp[n][m]=min(1+solve(n-1,m,w1,w2,dp),min(1+solve(n,m-1,w1,w2,dp),1+solve(n-1,m-1,w1,w2,dp)));
-            }
+int minop(int i , int j, string word1 , string word2 , vector<vector<int>>&dp){
+    int n= word1.size();
+    int m= word2.size();
+    if(j==m){
+        return n-i;
+    }
+    if(i==n){
+        return m-j;
+    }
+    if(dp[i][j]!=-1) return dp[i][j];
+    if(word1[i]==word2[j]){
+        return dp[i][j]= minop(i+1,j+1,word1,word2,dp);
+    }
+    else{
+        int insert= 1+minop(i,j+1,word1,word2,dp);
+        int del= 1+minop(i+1,j,word1,word2,dp);
+        int rep=1+minop(i+1,j+1,word1,word2,dp);
+        return min({insert, del , rep});
+    }
+}
     int minDistance(string word1, string word2) {
-        int n=word1.size();
-        int m =word2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,word1,word2,dp);
-
-        
+        int n= word1.size();
+        int m=word2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,INT_MAX));
+        // return minop(0,0,word1,word2,dp);
+        for(int i=0;i<=n;i++){
+            dp[i][m]=(n-i);
+        }
+        for(int i=0;i<=m;i++){
+            dp[n][i]=(m-i);
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(word1[i]==word2[j]){
+                    dp[i][j]=dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j]=1+ min({dp[i+1][j+1], dp[i+1][j],dp[i][j+1]});
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
