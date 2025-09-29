abbc2b7748b9d6877dbea2b9d6375ccf0901fc4a
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-30 01:00:34 +0530

    Time: 5 ms (26.34%), Space: 19.8 MB (22.35%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,7 +4,7 @@ public:
         int n=prices.size();
         vector<vector<int>>dp(n+1,vector<int>(2,0));
         for(int i=n-1;i>=0;i--){
-            for(int j=0;j<=1;j++){
+            for(int j=1;j>=0;j--){
                 if(j==0){
                     int take= -prices[i]+dp[i+1][1];
                     int nottake= dp[i+1][0];
