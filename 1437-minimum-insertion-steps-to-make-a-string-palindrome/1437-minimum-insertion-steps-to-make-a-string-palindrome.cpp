bf3c382a431d7a025c097be76086dcbc69bff604
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-29 14:01:10 +0530

    Time: 27 ms (72.81%), Space: 31.4 MB (60.78%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,48 +1,22 @@
 class Solution {
 public:
     int minInsertions(string s) {
-        string text1=s;
-        reverse(s.begin(),s.end());
-        string text2 = s;
-        int n= text1.size();
-        int m = text2.size();
+        string t =s;
+        reverse(t.begin(),t.end());
+        int n= s.size();
+        int m=t.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<=n;i++){
-            dp[i][m]=0;
-        }
-        for(int j=0;j<=m;j++){
-            dp[n][j]=0;
-        }
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
-                if(text1[i]==text2[j]){
+                if(s[i]==t[j]){
                     dp[i][j]=1+dp[i+1][j+1];
                 }
                 else{
-                    dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+                    dp[i][j]= max(dp[i+1][j],dp[i][j+1]);
                 }
             }
         }
         return n-dp[0][0];
+        
     }
 };
-
-// class Solution {
-// public:
-//     int solve( int i , int j , string s , string t, vector<vector<int>>&dp){
-//         if(i<=0 || j<=0 ) return 0;
-//         if(dp[i][j]!=-1) return dp[i][j];
-//         if(s[i-1]== t[j-1]) return dp[i][j]=1+solve(i-1,j-1,s,t,dp);
-//         int val = max(solve(i-1,j,s,t,dp),solve(i,j-1,s,t,dp));
-//         return dp[i][j]= val;
-//     }
-//     int minInsertions(string s) {
-//         int n= s.size();
-//         string t;
-//         for(int i=n-1;i>=0;i--){
-//             t+=s[i];
-//         }
-//         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-//         return n- solve(n,n,s,t,dp);
-//     }
-// };
