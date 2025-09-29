a3659df10ef4451c45bd9153f04fd3c713806fd3
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-29 14:37:29 +0530

    Time: 30 ms (59.61%), Space: 44.3 MB (22.18%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
+int count(int i, int j , string s , string p,vector<vector<double>>&dp){
+    int n=s.size();
+    int m= p.size();
+    if(j==m) return 1;
+    if(i==n) return 0;
+    if(dp[i][j]!=-1) return dp[i][j];
+    if(s[i]==p[j]){
+        return dp[i][j]= count(i+1,j+1,s,p,dp)+count(i+1,j,s,p,dp);
+    }
+    return dp[i][j]= count(i+1,j,s,p,dp);
+}
     int numDistinct(string text1, string text2) {
         int n= text1.size();
         int m = text2.size();
+        if(n<m) return 0;
         vector<vector<double>>dp(n+1,vector<double>(m+1,0));
+        // return count(0,0,text1,text2,dp);
         for(int i=0;i<=n;i++) dp[i][m]=1;
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
