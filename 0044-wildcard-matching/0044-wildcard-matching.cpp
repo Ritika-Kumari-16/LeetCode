a220eff3002637a6fc9a71dcdfb98a8488fb38cf
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-29 15:59:23 +0530

    Time: 31 ms (64.61%), Space: 14.8 MB (66.16%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
+bool match(int i , int j , string s , string p , vector<vector<int>>&dp){
+    int n=s.size();
+    int m=p.size();
+    if(j==m){
+        return i==n;
+    }
+    if(i==n){
+        for(int k=j;k<m;k++){
+            if(p[k]!='*') return 0;
+        }
+        return 1;
+    }
+    if(dp[i][j]!=-1) return dp[i][j];
+    if(s[i]==p[j] || p[j]=='?'){
+        return dp[i][j]=match(i+1,j+1,s,p,dp);
+    }
+    else if(p[j]!='*') return dp[i][j]=0;
+    return dp[i][j]= (match(i,j+1,s,p,dp) || match(i+1,j,s,p,dp));
+}
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+        vector<vector<bool>> dp(n +1, vector<bool>(m +1, false));
+        // return match(0,0,s,p,dp);
         dp[n][m]=true;
         for(int i=m-1;i>=0;i--){
-            if(p[i]=='*'){
-                dp[n][i]=dp[n][i+1];
-            }
-            else{
-                break;
-            }
+            if(p[i]=='*') dp[n][i]=dp[n][i+1];
+            else dp[n][i]=false;
         }
-
-        for(int i= n-1 ;i>=0;i--){
+        for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
                 if(s[i]==p[j] || p[j]=='?'){
-                    dp[i][j] = dp[i+1][j+1];
+                    dp[i][j]=dp[i+1][j+1];
                 }
-                else if(p[j] == '*'){
-                    dp[i][j]=(dp[i][j+1] || dp[i+1][j+1] || dp[i+1][j]);
+                else if(p[j]!='*'){
+                    dp[i][j]=false;
                 }
                 else{
-                    dp[i][j]=false;
+                    dp[i][j]= (dp[i+1][j] || dp[i][j+1]);
                 }
             }
         }
