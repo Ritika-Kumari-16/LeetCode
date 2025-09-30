a02f16ffdc8c48018dd3b8f6089567ac15ac2b98
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-30 15:14:20 +0530

    Time: 72 ms (55.6%), Space: 14.4 MB (63.83%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,18 +1,70 @@
 class Solution {
 public:
+int maxlen(int i, int lastidx , vector<int>&nums,vector<vector<int>>&dp){
+    int n= nums.size();
+    if(i==n+1) return 0;
+    if(dp[i][lastidx]!=-1) return dp[i][lastidx];
+    int take=0;
+    if(lastidx==0 || nums[lastidx-1]<nums[i-1]){
+        take=1+maxlen(i+1,i,nums,dp);
+    }
+    int nottake=maxlen(i+1,lastidx,nums,dp);
+    return dp[i][lastidx]=max(take,nottake);
+}
     int lengthOfLIS(vector<int>& nums) {
         int n= nums.size();
-        vector<int>dp(n,1);
-        int maxi=1;
+        vector<int>dp(n+1,1);
+        // return maxlen(1,0,nums,dp);
+        int maxlen1=1;
         for(int i=0;i<n;i++){
             for(int previdx=0;previdx<i;previdx++){
-                if(nums[previdx]<nums[i]){
-                    dp[i]=max(dp[i],1+dp[previdx]);
-                    maxi=max(maxi,dp[i]);
+                if(nums[previdx]<nums[i] && dp[i]<1+dp[previdx]){
+                    dp[i]= 1+dp[previdx];
+                    maxlen1=max(maxlen1,dp[i]);
+
                 }
             }
         }
-        return maxi;
+        return maxlen1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // vector<int>dp(n,1);
+        // int maxi=1;
+        // for(int i=0;i<n;i++){
+        //     for(int previdx=0;previdx<i;previdx++){
+        //         if(nums[previdx]<nums[i]){
+        //             dp[i]=max(dp[i],1+dp[previdx]);
+        //             maxi=max(maxi,dp[i]);
+        //         }
+        //     }
+        // }
+        // return maxi;
     }
 };
 
