60cc71df1130c45a345e5755c75dc17cc106ccae
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-09-30 15:57:03 +0530

    Time: 10 ms (71.4%), Space: 12.5 MB (93.71%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -2,31 +2,31 @@ class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int n = nums.size();
-        vector<int> dp(n, 1);
-        vector<int> hash(n, 0);
-        int lastidx = 0;
-        int maxi = 1;
-        for (int i = 0; i < n; i++) {
-            hash[i] = i;
-            for (int previdx = 0; previdx < i; previdx++) {
-                if ((nums[i]%nums[previdx] ==0) && dp[i] < 1 + dp[previdx]) {
-                    dp[i] = 1 + dp[previdx];
-                    hash[i] = previdx;
+        int n= nums.size();
+        vector<int>dp(n,1);
+        vector<int>parent(n,0);
+        int idx=0;
+        int maxlen=0;
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            for(int j=0;j<i;j++){
+                if(((nums[i] % nums[j] )==0)&& dp[i]<1+dp[j]){
+                    dp[i]=1+dp[j];
+                    parent[i]=j;
                 }
             }
-            if (maxi < dp[i]) {
-                lastidx = i;
-                maxi = dp[i];
+            if(dp[i]>maxlen){
+                maxlen=dp[i];
+                idx=i;
             }
         }
-        vector<int> ans;
-        ans.push_back(nums[lastidx]);
-        while (lastidx != hash[lastidx]) {
-            lastidx = hash[lastidx];
-            ans.push_back(nums[lastidx]);
+        vector<int>ans;
+        ans.push_back(nums[idx]);
+        while(parent[idx]!=idx){
+            idx=parent[idx];
+            ans.push_back(nums[idx]);
         }
-        reverse(ans.begin(), ans.end());
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
