c5de20bb479647916ed3c376c995492abf8efb20
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-10-11 17:28:24 +0530

    Time: 124 ms (42.45%), Space: 114.4 MB (73.09%) - LeetHub

--- 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.cpp
+++ 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     vector<int> avoidFlood(vector<int>& rains) {
-        vector<int> ans(rains.size(), 1);
-        set<int> st;
-        unordered_map<int, int> mp;
-        for (int i = 0; i < rains.size(); ++i) {
-            if (rains[i] == 0) {
+        int n= rains.size();
+        vector<int>ans(n,1);
+        unordered_map<int,int>mpp;
+        set<int>st;
+        for(int i=0;i<n;i++){
+            if(rains[i]==0){
                 st.insert(i);
-            } else {
-                ans[i] = -1;
-                if (mp.count(rains[i])) {
-                    auto it = st.lower_bound(mp[rains[i]]);
-                    if (it == st.end()) {
+            }
+            else{
+                ans[i]=-1;
+                if(mpp.count(rains[i])){
+                    auto idx = st.lower_bound(mpp[rains[i]]);
+                    if(idx==st.end()){
                         return {};
                     }
-                    ans[*it] = rains[i];
-                    st.erase(it);
+                    else{
+                        ans[*idx]=rains[i];
+                        st.erase(idx);
+                    }
                 }
-                mp[rains[i]] = i;
+                mpp[rains[i]]=i;
+                
             }
         }
         return ans;
