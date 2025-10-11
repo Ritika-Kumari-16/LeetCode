6bb3b9f1e35072a50e24013b6d28a489f986913b
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-10-11 15:28:32 +0530

    Time: 144 ms (40.59%), Space: 151.9 MB (20.79%) - LeetHub

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
+int maxenergy(int i , vector<int>&energy , int k){
+    int n= energy.size();
+    if(i>=n) return 0;
+    //take energyy
+    int take=energy[i]+maxenergy(i+k,energy,k);
+    //nottake
+    int nottake=maxenergy(i+1,energy,k);
+    return max(take,nottake);
+}
     int maximumEnergy(vector<int>& energy, int k) {
-        int n = energy.size();
-        vector<int> dp(n);
-        int result = INT_MIN;
-        for (int i = n - 1; i >= 0; --i) {
-            dp[i] = energy[i] + ((i + k < n) ? dp[i + k] : 0);
-            result = max(result, dp[i]);
+        int n= energy.size();
+        vector<int>dp(n+1,0);
+        int maxenergyans=INT_MIN;
+        for(int i=n-1;i>=0;i--){
+            int en= energy[i];
+            if(i+k<n) en+=dp[i+k];
+            dp[i]=en;
+            maxenergyans=max(maxenergyans,en);
         }
-        return result;
+        return maxenergyans;
     }
 };
