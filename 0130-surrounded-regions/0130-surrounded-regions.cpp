157ea1dc98cab56a53a06b46d16af285e857fcfb
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-10-12 18:57:42 +0530

    Time: 0 ms (100%), Space: 14.4 MB (72.17%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,43 +1,45 @@
 class Solution {
 public:
-void dfs(int i , int j ,  vector<vector<char>>&board ,vector<vector<int>>&visited ){
-    visited[i][j]=1;
-    int drow[]={-1,0,0,1};
-    int dcol[]={0,-1,1,0};
-    for(int k=0;k<4;k++){
-        int nrow=i+drow[k];
-        int ncol=j+dcol[k];
-        if(nrow<0 || nrow>=board.size() || ncol<0 || ncol>=board[0].size() || visited[nrow][ncol] || board[nrow][ncol]=='X') continue;
-        dfs(nrow,ncol,board,visited);
+void dfs(int row , int col , vector<vector<char>>&board , vector<vector<int>>&visited){
+    visited[row][col]=1;
+    int delrow[]={-1,0,0,1};
+    int delcol[]={0,-1,1,0};
+    int n= board.size();
+    int m=board[0].size();
+    for(int i=0;i<4;i++){
+        int nrow=delrow[i]+row;
+        int ncol=delcol[i]+col;
+        if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !visited[nrow][ncol] && board[nrow][ncol]=='O'){
+            dfs(nrow,ncol,board,visited);
+        }
     }
 }
     void solve(vector<vector<char>>& board) {
-        int n= board.size();
+        int n =board.size();
         int m= board[0].size();
         vector<vector<int>>visited(n,vector<int>(m,0));
         for(int i=0;i<m;i++){
-            if(board[0][i]=='O' && !visited[0][i]){
+            if(board[0][i]=='O'){
                 dfs(0,i,board,visited);
             }
-            if(board[n-1][i]=='O' && !visited[n-1][i]){
+            if(board[n-1][i]=='O'){
                 dfs(n-1,i,board,visited);
             }
         }
-        for(int j=0;j<n;j++){
-            if(board[j][0]=='O'&& !visited[j][0]){
-                dfs(j,0,board,visited);
+        for(int i=0;i<n;i++){
+            if(board[i][0]=='O'){
+                dfs(i,0,board,visited);
             }
-            if(board[j][m-1]=='O' && !visited[j][m-1]){
-                dfs(j,m-1,board,visited);
+            if(board[i][m-1]=='O'){
+                dfs(i,m-1,board,visited);
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!visited[i][j] && board[i][j]=='O'){
+                if(board[i][j]=='O' && visited[i][j]==0){
                     board[i][j]='X';
                 }
             }
         }
-        
     }
 };
