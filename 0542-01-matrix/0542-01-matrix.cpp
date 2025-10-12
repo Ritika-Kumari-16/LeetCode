c080597a6ca4273ee2f934615b20b15d8497f37f
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-10-12 16:52:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m= mat[0].size();
-        vector<vector<int>>visited(n,vector<int>(m,0));
-        queue<pair<int,pair<int,int>>>q;
+        int n= mat.size();
+        int m = mat[0].size();
+        vector<vector<int>>ans(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(mat[i][j]==0){
+                if(mat[i][j]==1){
+                    queue<pair<int,pair<int,int>>>q;
+                    vector<vector<int>>visited(n,vector<int>(m,0));
                     q.push({0,{i,j}});
                     visited[i][j]=1;
+                    while(!q.empty()){
+                        auto it= q.front();
+                        q.pop();
+                        int row=it.second.first;
+                        int col=it.second.second;
+                        int dist=it.first;
+                        if(mat[row][col]==0){
+                            ans[i][j]=dist;
+                            break;
+                        }
+                        int delrow[]={-1,0,0,1};
+                        int delcol[]={0,-1,1,0};
+                        for(int k=0;k<4;k++){
+                            int nrow =delrow[k]+row;
+                            int ncol=delcol[k]+col;
+                            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !visited[nrow][ncol]){
+                                q.push({dist+1,{nrow,ncol}});
+                                visited[nrow][ncol]=1;
+                            }
+                        }
+                    }
                 }
             }
         }
-        int drow[]={-1,0,0,1};
-        int dcol[]={0,-1,1,0};
-        while(!q.empty()){
-            int dist=q.front().first;
-            int row= q.front().second.first;
-            int col=q.front().second.second;
-            q.pop();
-            for(int i=0;i<4;i++){
-                int nrow=row+drow[i];
-                int ncol=col+dcol[i];
-                if(nrow<0 || ncol<0 || nrow>=n || ncol>=m || visited[nrow][ncol] || mat[nrow][ncol]==0) continue;
-                q.push({dist+1,{nrow,ncol}});
-                visited[nrow][ncol]=1;
-                mat[nrow][ncol]=dist+1;
-            }
-
-        }
-        return mat;
+        return ans;
     }
 };
