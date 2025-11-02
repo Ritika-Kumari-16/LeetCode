3ee3903772186d6155b7c10b5ef998642a7f197c
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-11-02 10:28:15 +0530

    Time: 0 ms (100%), Space: 18.9 MB (27.03%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,36 +1,66 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>>adj(numCourses);
-        for(auto &it :prerequisites ){
-            adj[it[0]].push_back(it[1]);
-        }
-        vector<int>indegree(numCourses,0);
-        for(int i=0;i<numCourses;i++){
-            for(auto &it :adj[i]){
-                indegree[it]++;
-            }
-        }
-        queue<int>q;
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
+        vector<vector<int>>adjlist(numCourses);
+         vector<int>indeg(numCourses,0);
+        for(auto it:prerequisites){
+            adjlist[it[1]].push_back(it[0]);
+            indeg[it[0]]++;
+
         }
         vector<int>ans;
+        queue<int>q;
+        for(int i=0;i<numCourses;i++){
+            if(indeg[i]==0) q.push(i);
+        }
         while(!q.empty()){
             int node= q.front();
             q.pop();
             ans.push_back(node);
-            for(auto &it :adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0){
+            for(auto it:adjlist[node]){
+                indeg[it]--;
+                if(indeg[it]==0) {
                     q.push(it);
                 }
             }
         }
         if(ans.size()!=numCourses) return {};
-        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
+// class Solution {
+// public:
+//     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+//         vector<vector<int>>adj(numCourses);
+//         for(auto &it :prerequisites ){
+//             adj[it[0]].push_back(it[1]);
+//         }
+//         vector<int>indegree(numCourses,0);
+//         for(int i=0;i<numCourses;i++){
+//             for(auto &it :adj[i]){
+//                 indegree[it]++;
+//             }
+//         }
+//         queue<int>q;
+//         for(int i=0;i<numCourses;i++){
+//             if(indegree[i]==0){
+//                 q.push(i);
+//             }
+//         }
+//         vector<int>ans;
+//         while(!q.empty()){
+//             int node= q.front();
+//             q.pop();
+//             ans.push_back(node);
+//             for(auto &it :adj[node]){
+//                 indegree[it]--;
+//                 if(indegree[it]==0){
+//                     q.push(it);
+//                 }
+//             }
+//         }
+//         if(ans.size()!=numCourses) return {};
+//         reverse(ans.begin(),ans.end());
+//         return ans;
+//     }
+// };
