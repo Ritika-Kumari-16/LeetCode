20e240c2fc8064350d8510c5d4aa377173a4b427
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-11-02 10:26:06 +0530

    Time: 10 ms (26.04%), Space: 20.3 MB (23.28%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>>adj(numCourses);
-        for(auto &it :prerequisites ){
-            adj[it[0]].push_back(it[1]);
+        vector<vector<int>>adjlist(numCourses);
+         vector<int>indeg(numCourses,0);
+        for(auto it:prerequisites){
+            adjlist[it[1]].push_back(it[0]);
+            indeg[it[0]]++;
+
         }
-        vector<int>indegree(numCourses,0);
-        for(int i=0;i<numCourses;i++){
-            for(auto &it :adj[i]){
-                indegree[it]++;
-            }
-        }
+        vector<int>ans;
         queue<int>q;
         for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
+            if(indeg[i]==0) q.push(i);
         }
-        int courses=0;
         while(!q.empty()){
             int node= q.front();
             q.pop();
-            courses++;
-            for(auto &it :adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0){
+            ans.push_back(node);
+            for(auto it:adjlist[node]){
+                indeg[it]--;
+                if(indeg[it]==0) {
                     q.push(it);
                 }
             }
         }
-        return courses==numCourses;
+        return ans.size() ==numCourses;
     }
 };
