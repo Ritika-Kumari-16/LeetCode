009b0e70a97d36fe6756f73f5c05283829cb9b47
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-11-05 09:45:18 +0530

    Time: 43 ms (86.41%), Space: 24.5 MB (58.16%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    int minimumEffortPath(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m=grid[0].size();
-        vector<vector<int>>ans(n,vector<int>(m,INT_MAX));
-        ans[0][0]=0;
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int n=heights.size();
+        int m=heights[0].size();
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-        int drow[]={-1,0,0,1};
-        int dcol[]={0,-1,1,0};
+        int delrow[]={-1,0,0,1};
+        int delcol[]={0,-1,1,0};
+        vector<vector<int>>effortmat(n,vector<int>(m,1e9));
+        effortmat[0][0]=0;
         pq.push({0,{0,0}});
         while(!pq.empty()){
-            int dist=pq.top().first;
-            int row=pq.top().second.first;
-            int col=pq.top().second.second;
+            auto [effort,cell]=pq.top();
             pq.pop();
+            int row=cell.first;
+            int col=cell.second;
+            if(row==n-1 && col==m-1) return effort;
             for(int i=0;i<4;i++){
-                int nrow=row+drow[i];
-                int ncol=col+dcol[i];
-                if(nrow<0 || nrow>=n || ncol<0 || ncol>=m){
-                    continue;
-                }
-                int val=max(ans[row][col] , abs(grid[row][col]-grid[nrow][ncol]));
-                if(ans[nrow][ncol]> val){
-                    ans[nrow][ncol]=val;
-                    pq.push({val,{nrow,ncol}});
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
+                    int val=abs(heights[nrow][ncol]-heights[row][col]);
+                    int mineffort= max(effort,val);
+                    if(effortmat[nrow][ncol]> mineffort){
+                        effortmat[nrow][ncol]=mineffort;
+                        pq.push({mineffort,{nrow,ncol}});
+                    }
                 }
             }
         }
-
-        return ans[n-1][m-1];
-        
+        return -1;
     }
 };
