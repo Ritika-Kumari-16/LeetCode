1c47a4a9d82a1b8178d8f35ceb0e432cfd40dfd7
Ritika Yullam <141131985+Ritika-Kumari-16@users.noreply.github.com>
2025-11-06 10:38:57 +0530

    Time: 0 ms (100%), Space: 18.3 MB (51.9%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-         vector<vector<pair<int,int>>>adj(n);
-        for(auto &it :flights){
-            adj[it[0]].push_back({it[1],it[2]});
-            }
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<>>pq;
-        vector<int>ans(n,INT_MAX);
-        pq.push({0,{0,src}});
-        ans[src]=0;
+        vector<vector<pair<int,int>>>graph(n);
+        for(auto it : flights){
+            int from = it[0];
+            int to=it[1];
+            int cost=it[2];
+            graph[from].push_back({to,cost});
+        }
+        vector<int>dist(n,1e9);
+        dist[src]=0;
+        queue<pair<int,pair<int,int>>>pq;
+        //pq{cost,node,stops};
+        pq.push({0,{src,0}});
         while(!pq.empty()){
-            int kval=pq.top().first;
-            int price=pq.top().second.first;
-            int node =pq.top().second.second;
+            auto [cost,nodestops]=pq.front();
             pq.pop();
+            int node= nodestops.first;
+            int kval=nodestops.second;
             if(kval>k) continue;
-            for(auto &it: adj[node]){
+            for(auto it: graph[node]){
+                int price= it.second;
                 int adjnode= it.first;
-                int adjprice=it.second;
-                if(kval<=k && ans[adjnode]>price+adjprice){
-                    ans[adjnode]=price+adjprice;
-                    pq.push({kval+1,{ans[adjnode],adjnode}});
+                if(dist[adjnode]>cost+price){
+                    dist[adjnode]=cost+price;
+                    pq.push({cost+price,{adjnode,kval+1}});
                 }
             }
         }
-        return ans[dst]==INT_MAX ? -1 :ans[dst];
+        return (dist[dst]==1e9)? -1 :dist[dst];
     }
 };
